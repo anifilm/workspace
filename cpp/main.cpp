f0a899f65f7d08cb3d3e49bc8896bf4ade269345
anifilm <anifilm02@gmail.com>
2019-04-10 17:02:27 +0900

    clean .vscode

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,9 +2,20 @@
 
 using namespace std;
 
+// This progeram will clculate the area of a room in square feet
+
 int main() {
 
-	cout << "Hello, World!" << endl;
+	int room_width {0};
+	int room_length {0};
+
+	cout << "Enter the width of the room: ";
+	cin >> room_width;
+
+	cout << "Enter the length of thw room: ";
+	cin >> room_length;
+
+	cout << "The area of the room is " << room_width * room_length << " square feet" << endl;
 	
 	return 0;
 }
