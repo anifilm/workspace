67277d160e62400a93dc323831d5441f18490e68
anifilm <anifilm02@gmail.com>
2019-04-10 13:22:44 +0900

    study c

---
+++ c/pointer_passbyReference.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void square(int* const x);
+
+int main() {
+
+	int num = 9;
+
+	square(&num);
+	printf("The square of the given number is %d\n", num);
+
+	return 0;
+}
+
+void square(int* const x) {
+
+	*x = (*x) * (*x);
+}
