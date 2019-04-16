a8e0f7247f046e0906ac6d2b6cc657df3a3b0e57
anifilm <anifilm02@gmail.com>
2019-04-16 10:53:51 +0900

    study java

---
+++ java/Fifth/src/reference/Student2.java
@@ -0,0 +1,12 @@
+package reference;
+
+public class Student2 {
+
+    int studentID;
+    String studentName;
+    int koreaScore;
+    int mathScore;
+    // 과목 이름 변수 추가
+    String koreaSubject;
+    String mathSubject;
+}
