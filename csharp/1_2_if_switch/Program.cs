1252141d7b1733a2f6ebeef152fc304d006b1752
anifilm <anifilm02@gmail.com>
2019-04-19 01:16:10 +0900

    study csharp

---
+++ csharp/1_2_if_switch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1_2_if_switch
+{
+    class Program
+    {
+       static void convert(ref string color) {
+            switch(color) {
+
+                case "Red": color = "빨강"; break;
+                case "Green": color = "초록"; break;
+                case "Blue": color = "파랑"; break;
+                default: color = "알수 없는 색"; break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string s = "Red";
+            convert(ref s);
+            Console.WriteLine(s);
+        }
+    }
+}
--- csharp/First/Program.cs
+++ csharp/First/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Collections.Generic;
 
 namespace First
 {
@@ -8,11 +6,7 @@ namespace First
     {
         static void Main(string[] args)
         {
-            Dictionary<string, Action<TextWriter>> dic = new Dictionary<string, Action<TextWriter>>();
-
-            dic.Add("Sample1", (writer) => { writer.WriteLine("I'm sample1!");});
-            dic.Add("Sample2", (writer) => { writer.WriteLine("I'm sample2!");});
-            foreach(var item in dic.Values) item(Console.Out);
+            Console.WriteLine("Hello World!");
         }
     }
 }
