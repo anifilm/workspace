b718f3d2a0c380e418b462a75f18581c29c0b761
anifilm <anifilm02@gmail.com>
2019-04-19 15:01:48 +0900

    delete .vscode files

---
+++ csharp/Third/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Third
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
