87342749c200a223e3e2ef0cf8f11ecfa856ac45
anifilm <anifilm02@gmail.com>
2019-05-02 20:18:21 +0900

    study cpp

---
+++ cpp/section13/AccessModifiers.cpp
@@ -0,0 +1,29 @@
+// Section 13
+// Access Modifiers
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Player {
+private:
+    // attributes
+    string name {"Player"};
+    int health;
+    int xp;
+
+public:
+    // methods
+    void talk(string text_to_say) {cout << name << " says " << text_to_say << endl;}
+    bool is_dead();
+};
+
+int main() {
+    Player frank;
+    // frank.name = "Frank";                // compile error
+    // cout << frank.health << endl;        // compile error
+    frank.talk("Hello there");
+
+	return 0;
+}
--- cpp/section13/Friends/main.cpp
+++ cpp/section13/Friends/main.cpp
@@ -10,5 +10,6 @@ int main() {
 
 
 
+	cout << endl;	
 	return 0;
 }
