04298d1c68fc6e1f50b745087de1d0602f0175c5
anifilm <anifilm02@gmail.com>
2019-05-02 17:19:55 +0900

    study cpp

--- cpp/section12/Pointer_Challenge.cpp
+++ cpp/section12/Pointer_Challenge.cpp
@@ -39,11 +39,51 @@
 
 using namespace std;
 
+int *apply_all(const int *const arr1, size_t size1, const int *const arr2, size_t size2);
+void print(const int *const array, size_t size);
+
+int *apply_all(const int *const arr1, size_t size1, const int *const arr2, size_t size2) {
+	int *new_array {};
+	new_array = new int[size1 * size2];
+
+	int position {0};
+
+	for (size_t i {0}; i < size2; i++) {
+		for (size_t j {0}; j < size1; j++) {
+			new_array[position] = arr2[i] * arr1[j];
+            position++;
+		}
+	}
+
+	return new_array;
+}
+
+void print(const int *const array, size_t size) {
+	cout << "[ ";
+	for (size_t i {0}; i < size; i++)
+		cout << array[i] << " ";
+	cout << "]" << endl;
+}
+
 int main() {
+	const size_t array1_size {5};
+	const size_t array2_size {3};
 
+	int array1 [] {1, 2, 3, 4, 5};
+	int array2 [] {10, 20, 30};
 
+	cout << "Array 1: ";
+	print(array1, array1_size);
 
+	cout << "Array 2: ";
+	print(array2, array2_size);
 
-	cout << endl;	
+	int *results = apply_all(array1, array1_size, array2, array2_size);
+	constexpr size_t resutls_size {array1_size * array2_size};
+
+	cout << "Result: ";
+	print(results, resutls_size);
+
+	delete [] results;
 	return 0;
 }
