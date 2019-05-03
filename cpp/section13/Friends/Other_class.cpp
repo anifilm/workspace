3c7ba610a54dca441935b39c0423aa35eba62793
anifilm <anifilm02@gmail.com>
2019-05-03 16:34:20 +0900

    study cpp

---
+++ cpp/section13/Friends/Other_class.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Other_class.h"
+#include "Player.h"
+
+void Other_class::display_player(Player &p) {
+    std::cout << p.name << std::endl;
+    std::cout << p.health << std::endl;
+    std::cout << p.xp << std::endl;
+}
--- cpp/section13/Friends/main.cpp
+++ cpp/section13/Friends/main.cpp
@@ -1,15 +1,31 @@
 // Section 13
-// 
+// Friend
 
 #include <iostream>
+#include <string>
+#include "Player.h"
+#include "Other_class.h"
+#include "Friend_class.h"
 
 using namespace std;
 
+void display_player(Player &p) {
+	cout << p.name << endl;
+	cout << p.health << endl;
+	cout << p.xp << endl;
+}
+
 int main() {
 
+	Player hero("Hero", 100, 35);
+	display_player(hero);
 
+	Other_class other;
+	other.display_player(hero);
 
+	Friend_class friend_class;
+	friend_class.set_hero_name(hero, "Super hero");
+	friend_class.display_player(hero);
 
-	cout << endl;	
 	return 0;
 }
