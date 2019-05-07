e63d965b792b2d3afb5dccf2646e71a9a0a8f860
anifilm <anifilm02@gmail.com>
2019-05-07 13:18:11 +0900

    study cpp

---
+++ cpp/comp3200/1_2_PrintEverything/PrintEverything.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	cout << "+--------------------------------+" << endl;
+	cout << "|    Print Everything Example    |" << endl;
+	cout << "+--------------------------------+" << endl;
+
+	int integer = 10;
+	float decimal = 1.5f;
+	char letter = 'A';
+	char string[] = "Hello, world!";
+
+	cout << integer << endl;
+	cout << decimal << endl;
+	cout << letter << endl;
+	cout << string << endl;
+} 
