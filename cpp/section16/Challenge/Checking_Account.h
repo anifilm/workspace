67fce9333e53c48bc337c54443763c8a84b76cf5
anifilm <anifilm02@gmail.com>
2019-05-30 17:03:22 +0900

    study cpp

---
+++ cpp/section16/Challenge/Checking_Account.h
@@ -0,0 +1,17 @@
+#ifndef _CHCKING_ACCOUNT_H_
+#define _CHCKING_ACCOUNT_H_
+#include "Account.h"
+
+class Checking_Account: public Account {
+    friend std::ostream &operator<<(std::ostream &os, const Checking_Account &account);
+private:
+    static constexpr const char *def_name = "Unnamed Checking Account";
+    static constexpr double def_balance = 0.0;
+    static constexpr double per_check_fee = 1.50;
+public:
+    Checking_Account(std::string name = def_name, double balance = def_balance);
+    bool withdraw(double amount);
+    // Inherits the Account::withdraw methods
+};
+
+#endif // _CHCKING_ACCOUNT_H_
--- cpp/section16/Challenge/header.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MYSTRING_H_
-#define _MYSTRING_H_
-
-class Mystring {
-
-};
-
-#endif // _MYSTRING_H_
