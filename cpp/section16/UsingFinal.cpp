3d61f00e7ce32df0104d2e037f2c20d4aaae9037
anifilm <anifilm02@gmail.com>
2019-06-01 14:17:38 +0900

    study cpp

--- cpp/section16/UsingFinal.cpp
+++ cpp/section16/UsingFinal.cpp
@@ -14,7 +14,7 @@ public:
 
 class Derived: public Base {
 public:
-	virtual void say_hello() const override {
+	virtual void say_hello() const override final {
 		cout << "Hello - I'm Derived class object" << endl;
 	}
 };
@@ -34,12 +34,12 @@ void greetings(const Base &obj) {
 int main() {
 
 	Base b;
-	b.say_hello();			// Base::say_hello()
+	b.say_hello();					// Base::say_hello()
 
 	Derived d;
-	d.say_hello();			// Derived::say_hello()
+	d.say_hello();					// Derived::say_hello()
 
-	Base *p1 = new Base();	// Base::say_hello()
+	Base *p1 = new Base();			// Base::say_hello()
 	p1->say_hello();
 
 	Derived *p2 = new Derived();	// Derived::say_hello()
@@ -48,7 +48,7 @@ int main() {
 	Base *p3 = new Derived();		// Base::say_hello()	????	IMPORTANT !!!
 	p3->say_hello();
 
-	cout << "\n==============================" << endl;
+	cout << "\n===========================================" << endl;
 	greetings(b);
 	greetings(d);
 	greetings(*p1);
