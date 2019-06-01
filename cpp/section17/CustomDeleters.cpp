661d46e4a065e4c4b950e39e1b638fbd060b8f90
anifilm <anifilm02@gmail.com>
2019-06-01 14:07:33 +0900

    study cpp

--- cpp/section17/CustomDeleters.cpp
+++ cpp/section17/CustomDeleters.cpp
@@ -1,15 +1,41 @@
 // Section 17
-// 
+// Custom deleters
 
 #include <iostream>
+#include <memory>
 
 using namespace std;
 
+class Test {
+private:
+	int data;
+public:
+	Test(): data {0} { cout << "Test constructor (" << data << ")" << endl; }
+	Test(int data): data {data} { cout << "Test constructor (" << data << ")" << endl; }
+	int get_data() const { return  data; }
+	~Test() { cout << "Test destructor (" << data << ")" << endl; }
+};
+
+void my_deleter(Test *ptr) {
+	cout << "Using my custom function deleter" << endl;
+	delete ptr;
+}
+
 int main() {
 
+	{
+		// Using a function
+		shared_ptr<Test> ptr1 { new Test {100}, my_deleter };
+	}
 
+	cout << "\n===================================" << endl;
+	{
+		// Using a Lambda expression
+		shared_ptr<Test> ptr2 (new Test {1000}, [] (Test *ptr) {
+			cout << "Using my custom lambda deleter" << endl;
+		});
+	}
 
-
-	cout << endl;	
+	cout << endl;
 	return 0;
 }
--- cpp/section17/WeakPointers.cpp
+++ cpp/section17/WeakPointers.cpp
@@ -1,15 +1,39 @@
 // Section 17
-// 
+// Weak Pointer
 
 #include <iostream>
+#include <memory>
 
 using namespace std;
 
+class B;		// forward declaration
+
+class A {
+	shared_ptr<B> b_ptr;
+public:
+	void set_B(shared_ptr<B> &b) {
+		b_ptr = b;
+	}
+	A() { cout << "A Constructor" << endl; }
+	~A() { cout << "A Destructor" << endl; }
+};
+
+class B {
+	weak_ptr<A> a_ptr;		// make weak to break the strong curcular reference
+public:
+	void set_A(shared_ptr<A> &a) {
+		a_ptr = a;
+	}
+	B() { cout << "B Constructor" << endl; }
+	~B() { cout << "B Destructor" << endl; }
+};
+
 int main() {
 
+	shared_ptr<A> a = make_shared<A> ();
+	shared_ptr<B> b = make_shared<B> ();
+	a->set_B(b);
+	b->set_A(a);
 
-
-
-	cout << endl;	
 	return 0;
 }
