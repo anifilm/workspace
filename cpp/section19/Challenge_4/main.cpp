7ede59667d497fb3cd8798b1b796e25796f7abac
anifilm <anifilm02@gmail.com>
2019-06-06 14:35:31 +0900

    study cpp

--- cpp/section19/Challenge_4/main.cpp
+++ cpp/section19/Challenge_4/main.cpp
@@ -1,14 +1,45 @@
-// Section 19
-// 
+// Section 19 Challenge 4
+// Copy Romeo and Juliet with line numbers
 
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 int main() {
+	
+	ifstream in_file {"./Challenge_4/romeoandjuliet.txt"};
+	ofstream out_file {"./Challenge_4/romeoandjuliet_out.txt"};
 
+	if (!in_file) {
+		cerr << "Error opening input file" << endl;
+		return 1;
+	}
 
+	if (!out_file) {
+		cerr << "Error creating file" << endl;
+		return 1;
+	}
 
+	string line {};
+	int line_number {0};
+
+	while (getline(in_file, line)) {
+		if (line == "")
+			out_file << endl;
+		else {
+			++line_number;
+			out_file << setw(7) << left << line_number
+				     << line << endl;
+		}
+	}
+
+	cout << "File copied" << endl;
+
+	in_file.close();
+	out_file.close();
 
 	cout << endl;	
 	return 0;
