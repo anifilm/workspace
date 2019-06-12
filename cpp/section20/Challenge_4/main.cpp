e15becf05edd865b7a90a2ac46b5de843ce5bd4e
anifilm <anifilm02@gmail.com>
2019-06-12 16:37:03 +0900

    study cpp

--- cpp/section20/Challenge_4/main.cpp
+++ cpp/section20/Challenge_4/main.cpp
@@ -13,20 +13,24 @@ using namespace std;
 bool is_palindrome(const string &s) {
 	// You must implement this function
 	// Since we are learning the STL - use a deque to solve the problem
-	deque<char> d;
+	stack<char> stk;
+	queue<char> q;
 
 	for (char c: s)
-		if (std::isalpha(c))
-			d.push_back(std::toupper(c));
+		if (std::isalpha(c)) {
+			c = std::toupper(c);
+			q.push(c);
+			stk.push(c);
+		}
 
 	char c1 {};
 	char c2 {};
 
-	while (d.size() > 1) {
-		c1 = d.front();
-		c2 = d.back();
-		d.pop_front();
-		d.pop_back();
+	while (!q.empty()) {
+		c1 = q.front();
+		q.pop();
+		c2 = stk.top();
+		stk.pop();
 
 		if (c1 != c2)
 			return false;
