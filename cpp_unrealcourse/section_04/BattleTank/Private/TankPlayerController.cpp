380a6214b4e98a4ccc5d541d84ac0db64bc8faa9
anifilm <anifilm02@gmail.com>
2019-06-25 17:47:44 +0900

    study cpp_unrealcourse battletank

---
+++ cpp_unrealcourse/section_04/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    auto ControlledTank = GetControlledTank();
+    if (!ControlledTank)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("PlayerController not prossesing a tank"));
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("PlayerController prossessing: %s"), *(ControlledTank->GetName()));
+    }
+}
+
+void ATankPlayerController::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+    AimTowardsCrosshair();
+    //UE_LOG(LogTemp, Warning, TEXT("PlayerController ticking"));
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+    return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+    if (!GetControlledTank()) { return; }
+
+    // Get world location if linetrace through crosshair
+    // If it hits the landscape
+        // Tell controlled tank to aim at this point
+}
---
+++ cpp_unrealcourse/section_04/BattleTank/Public/TankPlayerController.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Tank.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"     // Must be the last include
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+    virtual void BeginPlay() override;
+    virtual void Tick(float DeltaTime) override;
+
+    ATank* GetControlledTank() const;
+
+    // Start the tank moving the barrel so that a shot would hit where
+    // the crosshair intersects the world
+    void AimTowardsCrosshair();
+};
