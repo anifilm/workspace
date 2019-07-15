38499f5b4f178b0719cb8fe51deb5a78f65fa760
anifilm <anifilm02@gmail.com>
2019-07-15 17:53:29 +0900

    study csharp

---
+++ csharp/section2/Challenge/Challenge/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte myByte = 25;               // unsigned integer
+            Console.WriteLine(myByte);
+
+            sbyte mySbyte = -15;            // signed integer
+            Console.WriteLine(mySbyte);
+
+            int myInt = -12318925;          // signed integer
+            Console.WriteLine(myInt);
+
+            uint myUint = 123091;           // unsigned integer
+            Console.WriteLine(myUint);
+
+            short myShort = -32768;         // signed integer
+            Console.WriteLine(myShort);
+
+            ushort myUShort = 65535;        // unsigned integer
+            Console.WriteLine(myUShort);
+
+            float myFloat = -31.1289f;      // single-precision floating point
+            Console.WriteLine(myFloat);
+
+            double myDouble = -12.1231250;  // double-precision floating point
+            Console.WriteLine(myDouble);
+
+            char myCharacter = 'A';         // single unicode character
+            Console.WriteLine(myCharacter);
+
+            bool myBool = true;             // logical boolean type
+            Console.WriteLine(myBool);
+
+            string myText = "I control text";   // sequence of characters
+            Console.WriteLine(myText);
+
+            string numText = "15";
+            int myNumFromText = int.Parse(numText);
+            Console.WriteLine(myNumFromText);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ csharp/section2/Constants/Constants/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constants
+{
+    // Constants are immutable values which are known
+    // at compile time and do not change for the life of the program.
+
+    class Program
+    {
+        // constants as fields
+        const double PI = 3.14159265359;
+        const int weeks = 52, months = 12;
+
+        // Create a constant of type string with your birthday as its value
+        const string birthday = "1978.09.18";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My birthday is always going to be: {0}", birthday);
+            Console.ReadKey();
+        }
+    }
+}
