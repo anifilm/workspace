6d10503ab2795aeb94e74266846211a613005d1a
anifilm <anifilm02@gmail.com>
2019-07-16 16:59:07 +0900

    study csharp

---
+++ csharp/section3/UserInput/UserInput/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calulate());
+            Console.Read();
+        }
+
+        public static int Calulate()
+        {
+            Console.Write("Please enter the first number: ");
+            string numberInput1 = Console.ReadLine();
+            Console.Write("Please enter the second number: ");
+            string numberInput2 = Console.ReadLine();
+
+            int num1 = int.Parse(numberInput1);
+            int num2 = int.Parse(numberInput2);
+
+            int result = num1 + num2;
+            return result;
+        }
+    }
+}
