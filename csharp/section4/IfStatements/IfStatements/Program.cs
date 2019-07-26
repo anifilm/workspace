ea50d2d05a57c57b5d3b45bec821354bf29d3792
anifilm <anifilm02@gmail.com>
2019-07-26 16:05:36 +0900

    study csharp

---
+++ csharp/section4/IfStatements/IfStatements/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's the temperature like?");
+            string temperature = Console.ReadLine();
+
+            int numTemp;
+            int number;
+            bool userEnteredANumber = int.TryParse(temperature, out number);
+
+            if (userEnteredANumber)
+            {
+                numTemp = number;
+            }
+            else
+            {
+                numTemp = 0;
+                Console.WriteLine("Value entered, was  no number. 0 set as temperature.");
+            }
+
+            if (numTemp < 20)
+            {
+                Console.WriteLine("Take the coat");
+            }
+            else if (numTemp == 20)
+            {
+                Console.WriteLine("Pants and Pull Over should be fine");
+            }
+            else if (numTemp > 20)
+            {
+                Console.WriteLine("Shorts are enough today");
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ csharp/section4/IfStatements2/IfStatements2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfStatements2
+{
+    class Program
+    {
+        static int highscore = 300;
+        static string highscorePlayer = "Denis";
+
+        static void Main(string[] args)
+        {
+            CheckHighscore(250, "Gil");
+            CheckHighscore(315, "Hung");
+            CheckHighscore(350, "Lim");
+
+            Console.Read();
+        }
+
+        public static void CheckHighscore(int score, string playerName)
+        {
+            if (score > highscore)
+            {
+                highscore = score;
+                highscorePlayer = playerName;
+
+                Console.WriteLine("New highscore is " + score);
+                Console.WriteLine("It is now held by " + playerName);
+            }
+            else
+            {
+                Console.WriteLine("The old highscore could not be broken. It is still " + highscore + " and held by " + highscorePlayer);
+            }
+        }
+    }
+}
