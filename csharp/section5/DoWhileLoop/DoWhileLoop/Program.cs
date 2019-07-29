457c0986b8e79f4c3d7832b728393a3599246e7d
anifilm <anifilm02@gmail.com>
2019-07-29 18:03:22 +0900

    study csharp

---
+++ csharp/section5/DoWhileLoop/DoWhileLoop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengthOfText = 0;
+            string wholeText = "";
+
+            do
+            {
+                Console.WriteLine("Please enter the name of a friend");
+                string nameOfAFriend = Console.ReadLine();
+                int currnetLenght = nameOfAFriend.Length;
+                lengthOfText += currnetLenght;
+                wholeText += nameOfAFriend;
+            }
+            while (lengthOfText < 20);
+
+            Console.WriteLine("Thanks, that was enough! " + wholeText);
+            Console.Read();
+        }
+    }
+}
