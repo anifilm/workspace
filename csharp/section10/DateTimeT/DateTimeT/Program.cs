5ae4f4432d7bd65d0c6532afe70f566df818526f
anifilm <anifilm02@gmail.com>
2019-08-09 12:09:02 +0900

    study csharp

---
+++ csharp/section10/DateTimeT/DateTimeT/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime(1978, 9, 18);
+            
+            Console.WriteLine("My birthday is {0}", dateTime);
+
+            // Write today on screen
+            Console.WriteLine("\nToday is {0}", DateTime.Today);
+            Console.WriteLine("Today is {0}", DateTime.Today.DayOfWeek);
+
+            // Write current time on screen
+            Console.WriteLine("\nNow is {0}", DateTime.Now);
+
+            DateTime tomorrow = GetTomorrow();
+            Console.WriteLine("\nTomorrow will be the {0}", tomorrow);
+            Console.WriteLine(GetFirstDayOfYear(2020));
+
+            int days = DateTime.DaysInMonth(2000, 2);
+            Console.WriteLine("\nDays in Feb 2000: {0}", days);
+            days = DateTime.DaysInMonth(2002, 2);
+            Console.WriteLine("Days in Feb 2002: {0}", days);
+            days = DateTime.DaysInMonth(2004, 2);
+            Console.WriteLine("Days in Feb 2004: {0}", days);
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine("\nMinute: {0}", now.Minute);
+
+            // display the time in this structure x o'clock y minutes and z seconds
+            Console.WriteLine("{0} o'clock {1} minutes and {2} secounds", now.Hour, now.Minute, now.Second);
+
+            Console.WriteLine("\nWrite a date in this format: yyyy-mm-dd");
+            string input = Console.ReadLine();
+
+            if (DateTime.TryParse(input, out dateTime))
+            {
+                Console.WriteLine(dateTime);
+                TimeSpan daysPassed = now.Subtract(dateTime);
+                Console.WriteLine("\nDays passed since: {0}", daysPassed.Days);
+            }
+            else
+            {
+                Console.WriteLine("Wrong input!");
+            }
+
+            Console.ReadKey();
+        }
+
+        static DateTime GetTomorrow()
+        {
+            return DateTime.Today.AddDays(1);
+        }
+
+        static DateTime GetFirstDayOfYear(int year)
+        {
+            return new DateTime(year, 1, 1);
+        }
+    }
+}
---
+++ csharp/section10/NullablesE/NullablesE/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullablesE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? num1 = null;
+            int? num2 = 1337;
+            int num5;
+
+            double? num3 = new Double?();
+            double? num4 = 3.14157;
+
+            bool? boolval = new bool?();
+
+            Console.WriteLine("Our nullable numbers are: {0}, {1}, {2}, {3}", num1, num2, num3, num4);
+            Console.WriteLine("The nullable boolean value is {0}", boolval);
+
+            bool? isMale = null;
+
+            if (isMale == true)
+            {
+                Console.WriteLine("User is male");
+            }
+            else if (isMale == false)
+            {
+                Console.WriteLine("User is female");
+            }
+            else
+            {
+                Console.WriteLine("No gender chosen");
+            }
+
+            double? num6 = 13.1;
+            double? num7 = null;
+            double num8;
+
+            if (num6 == null)
+            {
+                num8 = 0.0;
+            }
+            else
+            {
+                num8 = (double)num6;
+            }
+
+            Console.WriteLine("Value of num8 is {0}", num8);
+
+            // Shorter: THE NULL COALESCING OPERATOR ??
+            num8 = num6 ?? 8.53;
+            Console.WriteLine("Value of num8 is {0}", num8);
+
+            // if num7 is null then num8 = 8.53
+            num8 = num7 ?? 8.53;
+            Console.WriteLine("Value of num8 is {0}", num8);
+
+
+            Console.ReadKey();
+        }
+    }
+}
