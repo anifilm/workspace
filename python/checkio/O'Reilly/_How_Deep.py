edeefb22d493faf23670a9199d2e83e3c4bc28ae
anifilm <anifilm02@gmail.com>
2020-05-25 09:57:38 +0900

    study algorithm 2020-05-25

---
+++ python/checkio/O'Reilly/_How_Deep.py
@@ -0,0 +1,19 @@
+def how_deep(structure):
+    # your code here
+    return 1
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(how_deep((1, 2, 3)))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert how_deep((1, 2, 3)) == 1
+    assert how_deep((1, 2, (3,))) == 2
+    assert how_deep((1, 2, (3, (4,)))) == 3
+    assert how_deep(()) == 1
+    assert how_deep(((),)) == 2
+    assert how_deep((((),),)) == 3
+    assert how_deep((1, (2,), (3,))) == 2
+    assert how_deep((1, ((),), (3,))) == 3
+    print("Coding complete? Click 'Check' to earn cool rewards!")
