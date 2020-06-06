1bc7eb12a25f4e1df72b42653dcc1b3387a36e32
anifilm <anifilm02@gmail.com>
2020-06-06 12:01:17 +0900

    study python 2020-06-06

--- python/NomadCoders/2_JobScrapper/main.py
+++ python/NomadCoders/2_JobScrapper/main.py
@@ -1,15 +1,7 @@
-import requests
-from bs4 import BeautifulSoup
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
-indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
+last_indeed_page = extract_indeed_pages()
 
-indeed_soup = BeautifulSoup(indeed_result.text, 'html.parser')
+indeed_jobs = extract_indeed_jobs(last_indeed_page)
 
-pagination = indeed_soup.find("div", {"class":"pagination"})
-
-links = pagination.find_all('a')
-pages = []
-for link in links[:-1]:
-    pages.append(int(link.string))
-
-max_page = pages[-1]
+# print(indeed_jobs)
