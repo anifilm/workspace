211c11fa9064a5faebfab0779b1b43b2c804186e
anifilm <anifilm02@gmail.com>
2020-06-06 20:16:04 +0900

    study python 2020-06-06

---
+++ python/NomadCoders/4_WebScrapper/main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect
+from scrapper import get_jobs
+
+app = Flask("WebScrapper")
+
+db = {}
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/report")
+def report():
+    word = request.args.get('word')
+    if word:
+        word = word.lower()
+        existingJobs = db.get(word)
+        if existingJobs:
+            jobs = existingJobs
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
+    else:
+        return redirect("/")
+    return render_template(
+        "report.html",
+        searchingBy=word,
+        resultsNumber=len(jobs),
+        jobs = jobs
+    )
+
+app.run()
