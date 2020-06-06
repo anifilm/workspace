59f2c10b63a7dd954905fcf5151aac4728620089
anifilm <anifilm02@gmail.com>
2020-06-06 17:54:51 +0900

    study python 2020-06-06

--- python/NomadCoders/2_JobScrapper/main.py
+++ python/NomadCoders/2_JobScrapper/main.py
@@ -1,7 +1,9 @@
-from indeed import extract_indeed_pages, extract_indeed_jobs
+from indeed import get_jobs as get_indeed_jobs
+from so import get_jobs as get_so_jobs
+from save import save_to_file
 
-last_indeed_page = extract_indeed_pages()
-
-indeed_jobs = extract_indeed_jobs(last_indeed_page)
-
-# print(indeed_jobs)
+indeed_jobs = get_indeed_jobs()
+# so_jobs = get_so_jobs()
+# jobs = so_jobs + indeed_jobs
+jobs = indeed_jobs
+save_to_file(jobs)
