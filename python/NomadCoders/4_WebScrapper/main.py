e700fa27ef6960eaf6f849c477c36dead2f35c13
anifilm <anifilm02@gmail.com>
2020-06-07 07:55:18 +0900

    study python 2020-06-07

--- python/NomadCoders/4_WebScrapper/main.py
+++ python/NomadCoders/4_WebScrapper/main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from scrapper import get_jobs
+from exporter import save_to_file
 
 app = Flask("WebScrapper")
 
@@ -28,5 +29,20 @@ def report():
         resultsNumber=len(jobs),
         jobs = jobs
     )
+
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get('word')
+        if not word:
+            raise Exception()
+        word = word.lower()
+        jobs = db.get(word)
+        if not jobs:
+            raise Exception()
+        save_to_file(jobs)
+        return send_file("jobs.csv")
+    except:
+        return redirect("/")
 
 app.run()
