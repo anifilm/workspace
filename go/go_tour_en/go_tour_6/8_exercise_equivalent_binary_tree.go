d3aecb1cd1a5d64706aa9d6b3469580f1a6d4b8c
anifilm <anifilm02@gmail.com>
2020-06-22 14:51:15 +0900

    study golang 2020-06-22

---
+++ go/go_tour_en/go_tour_6/8_exercise_equivalent_binary_tree.go
@@ -0,0 +1,16 @@
+// Exercise: Equivalent Binary Trees
+package main
+
+import "golang.org/x/tour/tree"
+
+// Walk walks the tree t sending all values
+// from the three to the channel ch.
+func Walk(t *tree.Tree, ch chan int)
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool
+
+func main() {
+
+}
