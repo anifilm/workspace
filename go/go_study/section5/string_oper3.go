67f4aca2f67a8253ebf2e05484d6d7be421b93d4
anifilm <anifilm02@gmail.com>
2020-06-27 22:29:18 +0900

    Update string_oper3.go

--- go/go_study/section5/string_oper3.go
+++ go/go_study/section5/string_oper3.go
@@ -14,14 +14,14 @@ func main() {
     str1 := "Go is expressive, concise, clean, and efficient. Its concurrency mechanisms make it easy to " +
         "write programs that get the most out of multicore and networked machines, while its novel " +
         "type system enables flexible and modular program construction. Go compiles quickly to machine " +
-        "code yet has the convenience of garbage collection and the power of run-time reflection. "
+        "code yet has the convenience of garbage collection and the power of run-time reflection."
     str2 := "It's a fast, statically typed, compiled language that feels like a dynamically typed, interpreted language."
 
-    fmt.Println("ex1:", str1+str2)
+    fmt.Println("ex1:", str1+" "+str2)
 
     // 예제 2 (결합: Join())
     strSet := []string{} // 슬라이스 선언
     strSet = append(strSet, str1)
     strSet = append(strSet, str2)
-    fmt.Println("ex2:", strings.Join(strSet, ""))
+    fmt.Println("ex2:", strings.Join(strSet, " "))
 }
