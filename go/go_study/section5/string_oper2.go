ddbedebcd14b1b908b860f78913bcb57b73ea3b8
anifilm <anifilm02@gmail.com>
2020-06-27 22:19:01 +0900

    Update string_oper2.go

--- go/go_study/section5/string_oper2.go
+++ go/go_study/section5/string_oper2.go
@@ -11,8 +11,9 @@ func main() {
     var str1 string = "Golang"
     var str2 string = "World"
 
-    fmt.Println("ex1:", str1 == str2) // 바이트로 비교
+    fmt.Println("ex1:", str1 == str2)
     fmt.Println("ex2:", str1 != str2)
     fmt.Println("ex3:", str1 > str2)
-    fmt.Println("ex4:", str1 < str2)
+    fmt.Println("ex4:", str1 < str2)  // Go 문자열 -> 아스키 코드에 대한 사전적 비교
+                                      // abc < ABC
 }
