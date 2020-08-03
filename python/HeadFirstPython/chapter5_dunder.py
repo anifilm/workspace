39fe189622478da3d89251799a2e13680d7db3d6
anifilm <anifilm02@gmail.com>
2020-08-03 11:19:27 +0900

    study python 2020-08-03

---
+++ python/HeadFirstPython/chapter5_dunder.py
@@ -0,0 +1,4 @@
+print('We start off in:', __name__)
+
+if __name__ == '__main__':
+    print('And end uo in:', __name__)
---
+++ python/HeadFirstPython/chapter5_webapp/vsearch4web_log.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, redirect, escape
+from vsearch import search4letters
+
+app = Flask(__name__)
+
+def log_request(reg: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req, res, file=log)
+
+@app.route('/search4', methods=['POST'])
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results:'
+    results = str(search4letters(phrase, letters))
+    log_request(request, results)
+    return render_template('results.html', the_phrase=phrase, the_letters=letters, the_title=title, the_results=results,)
+
+@app.route('/viewlog')
+def view_the_log() -> str:
+    with open('vsearch.log') as log
+        contents = log.read()
+    return escape(contents)
+
+@app.route('/')
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html', the_title='Welcome to search4letters on the web!')
+
+if __name__ == '__main__':
+    app.run(debug=True)
