2aadf89de2f5bc07f647e38291fef360dc142d6e
anifilm <anifilm02@gmail.com>
2020-08-05 12:15:06 +0900

    study clang 2020-08-05

---
+++ c/doit_c/05-2_sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int Sum(int value1, int value2)
+{
+    int result = value1 + value2;
+    return result;
+}
+
+void main()
+{
+    int s;
+    s = Sum(2, 3);
+    printf("더하기 작업의 결과는 %d\n", s);
+}
