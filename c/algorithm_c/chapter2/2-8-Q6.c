dd99b2c8559ce5c7fabb1e15baa4d0a282836ea3
anifilm <anifilm02@gmail.com>
2020-08-15 19:38:35 +0900

    study clang 2020-08-15

--- c/algorithm_c/chapter2/2-8-Q6.c
+++ c/algorithm_c/chapter2/2-8-Q6.c
@@ -1,32 +1,28 @@
 // Q6 실습 2-8의 card_convr 함수를 수정하여 배열의 맨 앞쪽에 아랫자리가 아니라 윗자리를 저장하는
 // card_conv 함수를 작성하세요.
 #include <stdio.h>
-#include <stdlib.h>
+
+// type 형의 x와 y의 값을 교환
+#define swap(type, x, y) do { type t = x; x = y; y = t; } while (0)
 
 // 정수값 x를 n진수로 변환하여 배열 d에 윗자리부터 저장
 int card_conv(unsigned x, int n, char d[]) {
     char dchar[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    int number = x;     // 변환할 값
     int digits = 0;     // 변환 후 자릿수
-    int digits2 = 0;
 
     if (x == 0)
         d[digits++] = dchar[0];
     else {
-        while (number) {
-            // 루프를 돌면서 변환 후 자릿수를 구함
-            number = number / n;
-            digits++;
-        }
-        digits2 = digits;
         while (x) {
-            // 나머지를 구하는 방법이 아닌 다른 방법이 있는지 확인 필요
-            // 현재는 루프를 돌면서 변환 후 자릿수를 구하여
-            // 아랫자리를 배열의 뒤쪽 인덱스부터 반대로 할당하는 방법으로 구현함
-            d[digits2--] = dchar[x % n];     // n으로 나눈 나머지를 저장
+            d[digits++] = dchar[x % n];     // n으로 나눈 나머지를 저장
             x /= n;
         }
     }
+
+    // d[0] ~ d[digits - 1]의 값을 역순으로 정렬합니다.
+    for (int i = 0; i < digits / 2; i++)
+        swap(char, d[i], d[digits - i - 1]);
+
     return digits;
 }
 
@@ -49,7 +45,7 @@ int main(void) {
         } while (cd < 2 || cd > 36);
         dno = card_conv(no, cd, cno);   // no를 cd진수로 변환
         printf("%d진수로는 ", cd);
-        for (i = 0; i <= dno; i++)  // 윗자리부터 차례대로 출력
+        for (i = 0; i < dno; i++)  // 윗자리부터 차례대로 출력
             printf("%c", cno[i]);
         printf(" 입니다.\n");
         printf("한번 더 할까요? (1...예 / 0...아니오): ");
