3498c3a4f2b2f915044c47c6c40d325ec0bffa41
anifilm <anifilm02@gmail.com>
2020-10-15 13:32:24 +0900

    study clang 2020-10-15

--- c/linux_system_c/2_file_lowlevel/open.c
+++ c/linux_system_c/2_file_lowlevel/open.c
@@ -8,11 +8,13 @@
 
 static int append_file() {
     int fd;
+    // datafile.txt 파일 쓰기모드 | append mode, 사용자 읽기쓰기 권한
     fd = open("datafile.txt", O_WRONLY | O_APPEND);
     if (fd == -1) {
         printf("open() error\n");
         return -1;
     }
+    // 유닉스 계열에서 lowlevel 파일 처리시 서식 지정하여 문자열을 파일에 쓰기
     dprintf(fd, "hello again..\n");
 
     close(fd);
@@ -21,11 +23,13 @@ static int append_file() {
 
 static int trunc_file() {
     int fd;
+    // datafile.txt 파일 쓰기모드 | 파일이 없으면 생성 | 파일이 있으면 내용을 초기화, 사용자 읽기쓰기 권한
     fd = open("datafile.txt", O_WRONLY | O_CREAT | O_TRUNC, 0600);
     if (fd == -1) {
         printf("open() error\n");
         return -1;
     }
+    // 유닉스 계열에서 lowlevel 파일 처리시 서식 지정하여 문자열을 파일에 쓰기
     dprintf(fd, "byebye file\n");
 
     close(fd);
@@ -34,29 +38,32 @@ static int trunc_file() {
 
 static int write_file() {
     int fd;
+    // datafile.txt 파일 쓰기모드 | 파일이 없으면 생성, 사용자 읽기쓰기 권한
     fd = open("datafile.txt", O_WRONLY | O_CREAT, 0600);
     if (fd == -1) {
         printf("open() error\n");
         return -1;
     }
+    // 유닉스 계열에서 lowlevel 파일 처리시 서식 지정하여 문자열을 파일에 쓰기
     dprintf(fd, "hello world!! %d\n", 123);
 
-    close(fd);
+    close(fd);      // 파일 닫기
     return 0;
 }
 
 int main(int argc, char **argv) {
 
+    // 파일에 쓰기 함수를 호출
     if (write_file()) {
         printf("write_file() error\n");
         return -1;
     }
-
+    // 파일을 초기화 이후 쓰기 함수를 호출
     if (trunc_file()) {
         printf("trunc_file() error\n");
         return -1;
     }
-
+    // 파일에 붙여쓰기 함수를 호출
     if (append_file()) {
         printf("append_file() error\n");
         return -1;
