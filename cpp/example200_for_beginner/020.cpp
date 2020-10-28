c1962c959e7b5cbbad4310e2f269ac12b984bdcb
anifilm <anifilm02@gmail.com>
2020-10-28 10:41:29 +0900

    study cpp 2020-10-28

--- cpp/example200_for_beginner/020.cpp
+++ cpp/example200_for_beginner/020.cpp
@@ -1,9 +1,23 @@
+// 논리형 변수 이해하기 (bool)
 #include <iostream>
 
 using namespace std;
 
 int main() {
 
+    int x {10};
+    int y {6};
+    bool is_true = false;
+
+    if (x > y)
+        is_true = true;
+    else
+        is_true = false;
+
+    if (is_true)
+        cout << "x는 y보다 큽니다." << endl;
+    else
+        cout << "x는 y보다 작습니다." << endl;
 
 
     return 0;
--- cpp/example200_for_beginner/021.cpp
+++ cpp/example200_for_beginner/021.cpp
@@ -1,10 +1,20 @@
+// 대입 연산자 이해하기 (=)
 #include <iostream>
 
 using namespace std;
 
 int main() {
 
+    int x {1};
+    int y {9};
+    int z = x + y;
 
+    double i {1.2};
+    double j {2.3};
+    double k = i + j;
+
+    cout << "x + y = " << z << endl;
+    cout << "i + j = " << k << endl;
 
     return 0;
 }
