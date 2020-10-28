19b60e287b6c46556acb673cda96c49305310e0b
anifilm <anifilm02@gmail.com>
2020-10-28 11:03:29 +0900

    Update 051.cpp

--- cpp/example200_for_beginner/051.cpp
+++ cpp/example200_for_beginner/051.cpp
@@ -1,3 +1,4 @@
+// 날짜와 시간을 문자열로 변환하기 (localtime)
 #include <iostream>
 #include <ctime>
 
@@ -8,11 +9,10 @@ int main() {
     time_t now = time(NULL);
     tm *ptm = localtime(&now);
 
-    char buffer[64];
-    strftime(buffer, 64, "예제 만드는 지금은 %Y년 %m월 %d일, %H시 %M분 %S초 입니다. (%p)", ptm);
+    char buffer[82];
+    strftime(buffer, 82, "예제 만드는 지금은 %Y년 %m월 %d일, %H시 %M분 %S초 입니다.(%p)", ptm);
 
     cout << buffer << endl;
-    cout << "test" << endl;
 
     return 0;
 }
