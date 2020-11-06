419c2e1827c54e05197bb96c5c2a4e06e07ee458
anifilm <anifilm02@gmail.com>
2020-11-06 15:19:34 +0900

    Update 103-2_pass_by_reference.c

--- c/udemy_c_TimBuchalka/103-2_pass_by_reference.c
+++ c/udemy_c_TimBuchalka/103-2_pass_by_reference.c
@@ -2,10 +2,8 @@
 #include <stdio.h>
 
 // fuction definition to swap the values
-void swap(int *x, int *y) {
-
+void swap(int* x, int* y) {
     int temp;
-
     temp = *x;  // save the value of x
     *x = *y;    // put y into x
     *y = temp;  // put temp into y
