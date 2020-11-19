489e8335469b74aaff95881055f73103445dbe08
anifilm <anifilm02@gmail.com>
2020-11-19 09:41:22 +0900

    study java 2020-11-19

---
+++ java/example200_for_beginner/s014_Hello.java
@@ -0,0 +1,7 @@
+public class s014_Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, world!");
+    }
+}
---
+++ java/example200_for_beginner/s015_JavaPrimaryType.java
@@ -0,0 +1,17 @@
+// 기본 타입 선언하기
+public class s015_JavaPrimaryType {
+
+    public static void main(String[] args) {
+
+        boolean isL = false;
+        char suit = 'H';
+        int year = 2020;
+        double latitude = 37.52127220511242;
+        double longgitude = 127.0074462890625;
+
+        System.out.println(isL);
+        System.out.println(suit);
+        System.out.println(year);
+        System.out.println(latitude + "\t" + longgitude);
+    }
+}
