c0c9d49ba126200cf9855d9b3cabad92156e2f0a
anifilm <anifilm02@gmail.com>
2020-11-29 16:36:28 +0900

    study cpp 2020-11-29

--- cpp/example200_for_beginner/125.cpp
+++ cpp/example200_for_beginner/125.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 template <typename T1, typename T2>
 string TempFunc(T1 x, T2 y) {
-//  conditional<is_same<T1, T2>::value, int, double >::type type1;  // 오류 확인 필요
-//  return typeid(type1).name();
+    conditional<is_same<T1, T2>::value, int, double >::type type1;
+    return typeid(type1).name();
 }
 
 int main() {
