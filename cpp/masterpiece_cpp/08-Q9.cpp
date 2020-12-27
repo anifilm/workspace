93dce3e7c67e82ac93fcb043a4c697ba622a6980
anifilm <anifilm02@gmail.com>
2020-12-27 21:12:46 +0900

    Update 08-Q9.cpp

--- cpp/masterpiece_cpp/08-Q9.cpp
+++ cpp/masterpiece_cpp/08-Q9.cpp
@@ -14,47 +14,132 @@ Q9
 
 using namespace std;
 
-class AirlineBook {
-private:
 
+class Console {
 public:
+    static void start();
+    static int menu();
+    static int time();
+    static int num();
+    static string name();
+};
 
+void Console::start() {
+    cout << "***** 한성항공에 오신것을 환영합니다 *****" << endl;
+}
+
+int Console::menu() {
+    int n;
+    cout << "\n예약(1), 취소(2), 보기(3), 끝내기(4) : ";
+    cin >> n;
+    return n;
+}
+
+int Console::time() {
+    int t;
+    cout << "07시(1), 12시(2), 17시(3) : ";
+    cin >> t;
+    return t;
+}
+
+int Console::num() {
+    int num;
+    cout << "좌석 번호: ";
+    cin >> num;
+    return num;
+}
+
+string Console::name() {
+    string name;
+    cout << "이름 입력: ";
+    cin >> name;
+    return name;
+}
+
+class Seat {
+private:
+    string seatName;
+public:
+    Seat() { seatName = "---"; }
+    string getName() { return seatName; }
+    void setName(string name) { seatName = name; }
 };
 
 class Schedule {
 private:
-
+    Seat* seat;
+    string time;
 public:
-
+    Schedule() { seat = new Seat[8]; }
+    ~Schedule() { delete[] seat; }
+    void setTime(string t) { time = t; }
+    void show() {
+        cout << time << ":\t";
+        for (int i = 0; i < 8; i++)
+            cout << seat[i].getName() << '\t';
+        cout << endl;
+    }
+    void reservationSeat(int num, string name) {
+        seat[num-1].setName(name);
+    }
+    void cancelSeat(int num, string name) {
+        seat[num-1].setName("---");
+    }
 };
 
-class Seat {
+class AirlineBook {
 private:
-
+    Schedule* schedule;
 public:
-
-};
-
-class Console {
-private:
-
-public:
-
+    AirlineBook() {
+        schedule = new Schedule[3];
+        schedule[0].setTime("07시");
+        schedule[1].setTime("12시");
+        schedule[2].setTime("17시");
+    }
+    ~AirlineBook() { delete[] schedule; }
+    void reservation(int t) {
+        schedule[t-1].show();
+        int num = Console::num();
+        string name = Console::name();
+        schedule[t-1].reservationSeat(num, name);
+    }
+    void cancel(int t) {
+        schedule[t-1].show();
+        int num = Console::num();
+        string name = Console::name();
+        schedule[t-1].cancelSeat(num, name);
+    }
+    void showAll() {
+        for (int i = 0; i < 3; i++)
+            schedule[i].show();
+    }
 };
 
 int main() {
 
-    AirlineBook* airlinebook = new AirlineBook();
+    AirlineBook airlinebook;
+    Console::start();
 
-    cout << "***** 한성항공에 오신것을 환영합니다 *****" << endl;
-
-    cout << "예약(1), 취소(2), 보기(3), 끝내기(4) : ";
-    cout << "07시(1), 12시(2), 17시(3) : ";
-    cout << "07시: ";
-    cout << "좌석 번호: ";
-    cout << "이름 입력: ";
-
-    cout << "예약 시스템을 종료합니다." << endl;
-
-    return 0;
+    while (true) {
+        int n = Console::menu();
+        switch (n) {
+            case 1: {
+                int t = Console::time();
+                airlinebook.reservation(t);
+                break;
+            }
+            case 2: {
+                int t = Console::time();
+                airlinebook.cancel(t);
+                break;
+            }
+            case 3:
+                airlinebook.showAll();
+                break;
+            case 4:
+                cout << "\n예약 시스템을 종료합니다." << endl;
+                return 0;
+        }
+    }
 }
