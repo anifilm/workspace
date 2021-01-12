50bb4d350c1a5d4552625ab1450950275a0dff10
anifilm <anifilm02@gmail.com>
2021-01-12 09:28:19 +0900

    study algorithm java 2021-01-12

--- java/algorithm_java/src/chap01/section2/Q8.java
+++ java/algorithm_java/src/chap01/section2/Q8.java
@@ -4,5 +4,23 @@
  */
 package chap01.section2;
 
+import java.util.Scanner;
+
 public class Q8 {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("1부터 n까지의 합을 구합니다.");
+        System.out.print("n의 값: ");
+        int n = stdIn.nextInt();
+
+        int sum = 0;    // 합
+        sum = (1+n) * n/2;
+        System.out.println("1부터 " + n + "까지의 합은 " + sum + "입니다.");
+
+        sum = 0;
+        for (int i = 1; i <= n; i++)
+            sum += i;
+        System.out.println("계산 검증: " + sum);
+    }
 }
--- java/algorithm_java/src/chap01/section2/Q9.java
+++ java/algorithm_java/src/chap01/section2/Q9.java
@@ -4,5 +4,31 @@ static int sumof(int a, int b)
  */
 package chap01.section2;
 
+import java.util.Scanner;
+
 public class Q9 {
+    static int sumof(int a, int b) {
+        int sum = 0;
+        if (a < b) {
+            for (int i = a; i <= b; i++)
+                sum += i;
+        }
+        else {
+            for (int i = b; i <= a; i++)
+                sum += i;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("a부터 b까지의 합을 구합니다.");
+        System.out.print("a의 값: ");
+        int a = stdIn.nextInt();
+        System.out.print("b의 값: ");
+        int b = stdIn.nextInt();
+
+        int sum = sumof(a, b);    // 합
+        System.out.println(a + "부터 " + b + "까지의 합은 " + sum + "입니다.");
+    }
 }
