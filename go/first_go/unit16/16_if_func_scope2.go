65052cdf3d270299509444e21c614aadba8a905a
anifilm <anifilm02@gmail.com>
2021-02-09 13:17:07 +0900

    Update 16_if_func_scope2.go

--- go/first_go/unit16/16_if_func_scope2.go
+++ go/first_go/unit16/16_if_func_scope2.go
@@ -12,4 +12,8 @@ func main() {
     } else {
         fmt.Println(err)
     }
+
+    // 변수 b와 err은 if 조 문의 안에서 선언되었기 때문에 if문 밖에서는 사용 불가
+    fmt.Println(b)   // 컴파일 에러, 변수 b를 사용할 수 없음
+    fmt.Println(err) // 컴파일 에러, 변수 err을 사용할 수 없음
 }
