b64168af2bac8db9b290a8577754a6505bfecc20
anifilm <anifilm02@gmail.com>
2021-02-17 11:20:12 +0900

    vue.js checkout L9-end

--- javascript/vue_mastery/intro-to-vue-3/main.js
+++ javascript/vue_mastery/intro-to-vue-3/main.js
@@ -2,41 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       cart: 0,
-      product: 'Socks',
-      brand: 'Vue Mastery',
-      selectedVariant: 0,
-      details: ['50% cotton', '30% wool', '20% polyster'],
-      variants: [
-        { id: 2234,
-          color: '#1ab371',
-          image: './assets/images/socks_green.jpg',
-          quantity: 50
-        },
-        { id: 2235,
-          color: '#31425e',
-          image: './assets/images/socks_blue.jpg',
-          quantity: 0
-        },
-      ]
+      premium: true
     }
   },
-  methods: {
-    addToCart() {
-      this.cart += 1
-    },
-    updateVariant(index) {
-      this.selectedVariant = index
-    }
-  },
-  computed: {
-    title() {
-      return this.brand + ' ' + this.product
-    },
-    image() {
-      return this.variants[this.selectedVariant].image
-    },
-    inStock() {
-      return this.variants[this.selectedVariant].quantity
-    }
-  }
+  methods: {}
 })
