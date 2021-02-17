9f9afb79c63f375885934293b4e642221ad728ca
anifilm <anifilm02@gmail.com>
2021-02-17 09:17:52 +0900

    vue.js checkout L5-end

--- javascript/vue_mastery/intro-to-vue-3/main.js
+++ javascript/vue_mastery/intro-to-vue-3/main.js
@@ -2,7 +2,13 @@ const app = Vue.createApp({
   data() {
     return {
       product: 'Socks',
-      image: './assets/images/socks_green.jpg'
+      image: './assets/images/socks_green.jpg',
+      inStock: true,
+      details: ['50% cotton', '30% wool', '20% polyster'],
+      variants: [
+        { id: 2234, color: 'green' },
+        { id: 2235, color: 'blue' }
+      ]
     }
   }
 })
