5818d78e09b250189b2395b8647643f0cbb76c66
anifilm <anifilm02@gmail.com>
2021-02-22 19:41:50 +0900

    range 수정

--- javascript/algorithm_js/chap02/section2/modules/range.js
+++ javascript/algorithm_js/chap02/section2/modules/range.js
@@ -1,47 +1,25 @@
 // range 배열 생성 함수
-const range = function (arg1, arg2, arg3) {
-  if (arguments.length == 1) {
-    let arr = [];
-    let start = 0, end = arg1
-    let length = end - start;
-    for (let i = 0; i < length; i++) {
-      arr[i] = start;
-      start++;
-    }
-    return arr;
+const range = function (start, stop, step) {
+  if (typeof stop == 'undefined') {
+    // one param defined
+    stop = start;
+    start = 0;
   }
-  else if (arguments.length == 2) {
-    let arr = [];
-    let start = arg1, end = arg2
-    let length = end - start;
-    for (let i = 0; i < length; i++) {
-      arr[i] = start;
-      start++;
-    }
-    return arr;
+
+  if (typeof step == 'undefined') {
+    step = 1;
   }
-  else if (arguments.length == 3 && arg3 < 0) {
-    let arr = [];
-    let start = arg1, end = arg2
-    let length = start - end;
-    for (let i = 0; i < length; i++) {
-      arr[i] = start;
-      start += arg3;
-      if (start < 0) break
-    }
-    return arr;
+
+  if ((step > 0 && start >= stop) || (step < 0 && start <= stop)) {
+    return [];
   }
-  else if (arguments.length == 3) {
-    let arr = [];
-    let start = arg1, end = arg2
-    let length = end - start;
-    for (let i = 0; i < length; i++) {
-      arr[i] = start;
-      start += arg3;
-      if (start > length) break
-    }
-    return arr;
+
+  let arr = [];
+  for (let i = start; step > 0 ? i < stop : i > stop; i += step) {
+    arr.push(i);
   }
+
+  return arr;
 }
 
 module.exports = range
