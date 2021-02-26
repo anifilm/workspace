fc1c85600f3167eab8bafead45e3d235ddc74e4e
anifilm <anifilm02@gmail.com>
2021-02-26 17:31:48 +0900

    update data

--- _study/john_ahn/react/index.js
+++ _study/john_ahn/react/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const config = require("./config/key");
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://anifilm:<password>@cluster0.70rsp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
