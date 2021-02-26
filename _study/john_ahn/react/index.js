262fbe145322c718ad7c95c9bdcd26c11450ede2
anifilm <anifilm02@gmail.com>
2021-02-26 17:22:51 +0900

    study react+express 2021-02-26

--- _study/john_ahn/react/index.js
+++ _study/john_ahn/react/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 5000
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://anifilm:lcy0200@cluster0.70rsp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://anifilm:<password>@cluster0.70rsp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
