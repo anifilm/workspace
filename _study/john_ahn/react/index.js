99bda5701131745da1aea49705772ec43b6b67ed
anifilm <anifilm02@gmail.com>
2021-02-26 18:33:45 +0900

    update data

--- _study/john_ahn/react/index.js
+++ _study/john_ahn/react/index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-const config = require('./config/key');
+const config = require('./config/key')
+
+const bodyParser = require('body-parser')
+const { User } = require('.models/User')
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -10,3 +13,8 @@ mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('register' (req, res) => {
+  const user = new User
+})
