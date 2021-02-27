ba1d34bc2d49d7598d44d0ec6ac6c9851fd7c26e
anifilm <anifilm02@gmail.com>
2021-02-27 14:48:27 +0900

    study js 2021-02-27

--- javascript/doit_js/chap09/js/getForm.js
+++ javascript/doit_js/chap09/js/getForm.js
@@ -1,5 +1,6 @@
-var selectMenu = document.testForm.major;  // 셀렉트 메뉴를 가져와 selectMenu로 저장
+var selectMenu = document.testForm.major;  // 선택 목록을 가져와 selectMenu로 저장
+
 function displaySelect() {
-  var selectedText = selectMenu.options[selectMenu.selectedIndex].innerText;
-  alert("[" + selectedText + "]를 선택했습니다.");
+  var selectedText = selectMenu.options[selectMenu.selectedIndex].innerText; // 선택한 옵션의 innerText를 가져와 selectedText에 저장
+  alert("[" + selectedText + "]를 선택했습니다."); // selectedText 내용을 알림 창에 표시
 }
