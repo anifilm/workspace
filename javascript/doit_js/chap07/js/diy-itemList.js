f9683f7b8707c8cbf2a708d7e578a0385037bc48
anifilm <anifilm02@gmail.com>
2021-03-05 09:12:13 +0900

    엔터키로 입력 가능하도록 수정

--- javascript/doit_js/chap07/js/diy-itemList.js
+++ javascript/doit_js/chap07/js/diy-itemList.js
@@ -1,36 +1,35 @@
-function addList() {
-  var item = document.querySelector("#item").value;
-  if (item != null) {
-    itemList.push(item);
-    document.querySelector("#item").value = "";
-    document.querySelector("#item").focus();
+function addList(e) {
+  e.preventDefault();
+  var item = document.querySelector('#item');
+  if (item.value != '') {
+    itemList.push(item.value);
   }
+  item.value = '';
+  item.focus();
   showList();
 }
 
 function showList() {
   // console.log(itemList);
-  var list = "<ul>";
+  var list = '<ul>';
   for (var i = 0; i < itemList.length; i++) {
-    list += "<li>" + itemList[i] + "<span class='close' id=" + i + ">X</span></li>";
+    list += '<li>' + itemList[i] + "<span class='close' id=" + i + '>X</span></li>';
   }
-  list += "</ul>";
-  document.querySelector("#itemList").innerHTML = list;
+  list += '</ul>';
+  document.querySelector('#itemList').innerHTML = list;
 
-  var remove = document.querySelectorAll(".close");
+  var remove = document.querySelectorAll('.close');
   for (var i = 0; i < remove.length; i++) {
-    remove[i].addEventListener("click", removeList);
+    remove[i].addEventListener('click', removeList);
   }
 }
 
 function removeList() {
   // console.log(this);
-  var id = this.getAttribute("id");
+  var id = this.getAttribute('id');
   itemList.splice(id, 1);
   showList();
 }
 
 var itemList = [];
-
-var addBtn = document.querySelector('#add');
-addBtn.addEventListener("click", addList);
+var addBtn = document.querySelector('form').addEventListener('submit', addList);
