899a1f43de2a1f64feb150700517a19b5b9daf4c
anifilm <anifilm02@gmail.com>
2021-03-08 15:54:37 +0900

    자료 추가

---
+++ java/spring_basic/hello-spring/src/main/java/hello/hellospring/controller/HelloControllter.java
@@ -0,0 +1,43 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloControllter {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "spring!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    @ResponseBody
+    public String helloMvc(@RequestParam("name") String name) {
+        return "hello " + name; // "hello spring"
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
---
+++ java/spring_basic/스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술.md
@@ -0,0 +1,22 @@
+스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술
+인프런 김영한
+
+
+spring initializr       https://start.spring.io/
+
+Project                 Language
+- Gradle Project        - Java
+
+Spring Boot
+- 2.3.9
+
+Project Metadata
+Group     hello
+Artifact  hello-spring
+
+Packaging - Jar
+Java      - 11
+
+Dependencies
++ Spring Web
++ Thymeleaf
