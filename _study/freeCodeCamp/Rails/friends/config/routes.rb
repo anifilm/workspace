e06fe09f7c47ea8b687bd3406f8912560b55642a
anifilm <anifilm02@gmail.com>
2021-03-08 15:02:18 +0900

    자료 정리

--- _study/freeCodeCamp/Rails/friends/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  resources :friends
-  #get 'home/index'
-  get 'home/about'
-  root 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
