7cfc02928013ef5dc5e5df4a06163a4bc1a3d319
anifilm <anifilm02@gmail.com>
2021-03-09 19:34:36 +0900

    study spring 2021-03-09

---
+++ java/spring_basic/hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,20 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.Test;
+
+public class MemoryMemberRepositoryTest {
+
+    MemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        System.out.println("result = " + (result == member));
+    }
+}
