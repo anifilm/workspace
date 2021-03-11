10767f2c4ae0e57820c224e3ec5599eaeede82c6
anifilm <anifilm02@gmail.com>
2021-03-11 18:12:17 +0900

    자료 정리

--- _study/_grammar/JavaScript/import_test/src/max.js
+++
@@ -1,7 +0,0 @@
-import { print, println } from './modules/print.js'
-import { len, min, max, sum } from './modules/array.js'
-import range from './modules/range.js'
-
-let arr = [1, 2, 3, 4, 5]
-
-println(sum(arr))
--- _study/_grammar/Ruby/readme.md
+++
@@ -1,2 +0,0 @@
-루비 처음 배우기
-https://smartbase.tistory.com/category/Ruby%20%26%20Rails
