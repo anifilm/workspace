7490a7c688425d4fd07f6989de4f4cd52293fcad
anifilm <anifilm02@gmail.com>
2021-03-15 10:24:34 +0900

    study react 2021-03-15

--- javascript/react_using_skill/first-app/src/4-2-3-1_EventPractice.js
+++ javascript/react_using_skill/first-app/src/4-2-3-1_EventPractice.js
@@ -2,20 +2,24 @@ import React, { Component } from 'react';
 
 class EventPractice extends Component {
   state = {
-    username: '',
     message: ''
   }
 
-  handleChange = (e) => {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(e) {
     this.setState({
-      [e.target.name]: e.target.value
+      message: e.target.value
     });
   }
 
-  handleClick = () => {
-    alert(this.state.username + ', ' + this.state.message);
+  handleClick() {
+    alert(this.state.message);
     this.setState({
-      username: '',
       message: ''
     });
   }
@@ -24,13 +28,6 @@ class EventPractice extends Component {
     return (
       <div>
         <h1>이벤트 연습</h1>
-        <input
-          type="text"
-          name="username"
-          placeholder="사용자명"
-          value={this.state.username}
-          onChange={this.handleChange}
-        />
         <input
           type="text"
           name="message"
--- javascript/react_using_skill/first-app/src/4-2-3-2_EventPractice.js
+++ javascript/react_using_skill/first-app/src/4-2-3-2_EventPractice.js
@@ -2,20 +2,18 @@ import React, { Component } from 'react';
 
 class EventPractice extends Component {
   state = {
-    username: '',
     message: ''
   }
 
   handleChange = (e) => {
     this.setState({
-      [e.target.name]: e.target.value
+      message: e.target.value
     });
   }
 
   handleClick = () => {
-    alert(this.state.username + ', ' + this.state.message);
+    alert(this.state.message);
     this.setState({
-      username: '',
       message: ''
     });
   }
@@ -24,13 +22,6 @@ class EventPractice extends Component {
     return (
       <div>
         <h1>이벤트 연습</h1>
-        <input
-          type="text"
-          name="username"
-          placeholder="사용자명"
-          value={this.state.username}
-          onChange={this.handleChange}
-        />
         <input
           type="text"
           name="message"
