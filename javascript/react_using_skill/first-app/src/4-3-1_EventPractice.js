85c5a1a1ba0573881067033bf6e80c8c07efea25
anifilm <anifilm02@gmail.com>
2021-03-15 15:10:53 +0900

    study react 2021-03-15

--- javascript/react_using_skill/first-app/src/4-3-1_EventPractice.js
+++ javascript/react_using_skill/first-app/src/4-3-1_EventPractice.js
@@ -1,24 +1,14 @@
 import React, { useState } from 'react';
 
 const EventPractice = () => {
-  const [form, setForm] = useState({
-    username: '',
-    message: ''
-  });
-  const { username, message } = form;
-  const onChange = e => {
-    const nextForm = {
-      ...form, // 기존의 form 내용을 이 자리에 복사한 뒤
-      [e.target.name]: e.target.value // 원하는 값을 덮어 씌우기
-    };
-    setForm(nextForm);
-  }
+  const [username, setUsername] = useState('');
+  const [message, setMessage] = useState('');
+  const onChangeUsername = e => setUsername(e.target.value);
+  const onChangeMessage = e => setMessage(e.target.value);
   const onClick = () => {
     alert(username + ': ' + message);
-    setForm({
-      username: '',
-      message: ''
-    });
+    setUsername('');
+    setMessage('');
   };
   const onKeyPress = e => {
     if (e.key === 'Enter') {
@@ -34,14 +24,14 @@ const EventPractice = () => {
       name="username"
       placeholder="사용자명"
       value={username}
-      onChange={onChange}
+      onChange={onChangeUsername}
       />
       <input
       type="text"
       name="message"
       placeholder="아무거나 입력해 보세요"
       value={message}
-      onChange={onChange}
+      onChange={onChangeMessage}
       onKeyPress={onKeyPress}
       />
       <button onClick={onClick}>확인</button>
