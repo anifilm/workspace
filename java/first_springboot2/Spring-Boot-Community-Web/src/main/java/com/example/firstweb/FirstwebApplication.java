0ee9c74632d0ce752a29de6442c2c18738b4d449
anifilm <anifilm02@gmail.com>
2021-03-23 19:33:25 +0900

    자료 추가

---
+++ java/first_springboot2/Spring-Boot-Community-Web/src/main/java/com/example/firstweb/FirstwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstwebApplication.class, args);
+    }
+
+}
