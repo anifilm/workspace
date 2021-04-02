cee1265642ea3452621566b7ec57692aac28cdeb
anifilm <anifilm02@gmail.com>
2021-04-02 19:48:44 +0900

    study java 2021-04-02

---
+++ java/masterpiece_java/src/chap10/section3/InnerClassListener.java
@@ -0,0 +1,46 @@
+package chap10.section3;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class InnerClassListener extends JFrame {
+    public InnerClassListener() {
+        setTitle("Action 이벤트 리스너 예제");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container c = getContentPane();
+        c.setLayout(new FlowLayout());
+
+        JButton btn = new JButton("Action");
+        btn.addActionListener(new MyActionListener());  // Action 이벤트 리스너 추가
+        c.add(btn);
+
+        setSize(350, 150);
+        setVisible(true);
+    }
+
+    // 내부 클래스로 Action 리스너를 작성
+    private class MyActionListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            JButton b = (JButton) e.getSource();
+            String title;
+            if (b.getText().equals("Action")) {
+                b.setText("액션");
+                title = "액션 이벤트 리스너 예제";
+            }
+            else {
+                b.setText("Action");
+                title = "Action 이벤트 리스너 예제";
+            }
+            // InnerClassListener의 멤버나 JFrame의 멤버를 호출할 수 있다.
+            InnerClassListener.this.setTitle(title);  // 프레임의 타이틀에 버튼 문자열을 출력
+        }
+    }
+
+    public static void main(String[] args) {
+        new InnerClassListener();
+    }
+}
+
