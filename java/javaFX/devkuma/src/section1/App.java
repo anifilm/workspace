37f28f882c575de704fd8eeab9f2604783015e76
anifilm <anifilm02@gmail.com>
2021-04-02 19:33:07 +0900

    study javaFX 2021-04-02

---
+++ java/javaFX/devkuma/src/section1/App.java
@@ -0,0 +1,23 @@
+package section1;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class App extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+        Label label = new Label("This is javaFX!");
+        FlowPane pane = new FlowPane();
+        pane.getChildren().add(label);
+        Scene scene = new Scene(pane, 320, 240);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
