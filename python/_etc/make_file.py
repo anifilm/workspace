1870419d5592720ba8b21c8f6af21298f15e0bc3
anifilm <anifilm02@gmail.com>
2021-04-05 17:20:15 +0900

    자료 추가

--- python/_etc/make_file.py
+++ python/_etc/make_file.py
@@ -1,7 +1,9 @@
 import os
 
-for i in range(1, 30):
+for i in range(10, 67):
     for j in range(1, 11):
-        f = open(str(i) + "-" + str(j) + "_.swift", "w")
-
+        f = open(str(i) + "-" + str(j) + "_.cs", "w")
+        f.write("using System\n")
+        f.write("\n")
+        f.write("//\n")
 f.close()
