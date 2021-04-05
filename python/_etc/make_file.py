a1dfabb3474a5770f512a6a140c81177de1c58c9
anifilm <anifilm02@gmail.com>
2021-04-05 17:07:31 +0900

    수정

--- python/_etc/make_file.py
+++ python/_etc/make_file.py
@@ -1,5 +1,7 @@
 import os
 
-for i in range(1, 11):
-    f = open("09-" + str(i) + "_.cs", "w")
-    f.close()
+for i in range(1, 30):
+    for j in range(1, 11):
+        f = open(str(i) + "-" + str(j) + "_.swift", "w")
+
+f.close()
--- python/_etc/make_folder.py
+++ python/_etc/make_folder.py
@@ -1,6 +1,6 @@
 import os
 
-for i in range(1, 21):
+for i in range(1, 30):
     if i < 10:
         os.mkdir("./chap0" + str(i))
     else:
