5c0da54003a095ef79f7a4ef06c119277837e829
anifilm <anifilm02@gmail.com>
2021-04-09 21:29:03 +0900

    study vue.js 2021-04-09

--- javascript/doit_vue_js/chap03/section2/quiz/js/app.js
+++ javascript/doit_vue_js/chap03/section2/quiz/js/app.js
@@ -1,21 +1,21 @@
 // 실습 #1 - `todo-footer` 컴포넌트 전역 등록
 // <p>This is another child global component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
-Vue.component('todo-footer', {
-    template: '<p>This is another child global component</p>'
+Vue.component("todo-footer", {
+  template: "<p>This is another child global component</p>",
 });
 
 // 실습 #2 - `todo-list` 컴포넌트 지역 등록
 // <p>This is another child local component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
 var cmp = {
-    template: '<p>This is another local child component</p>'
+  template: "<p>This is another local child component</p>",
 };
 
 var app = new Vue({
-    el: '#app',
-    data: {
-        message : 'This is a parent component'
-    },
-    components: {
-        'todo-list': cmp
-    }
+  el: "#app",
+  data: {
+    message: "This is a parent component",
+  },
+  components: {
+    "todo-list": cmp,
+  },
 });
