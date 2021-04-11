c98c88403e0d1345ba607d313d162ee999309343
anifilm <anifilm02@gmail.com>
2021-04-11 15:22:32 +0900

    study javascript 2021-04-11

--- _study/zerocho/js-es2020-webgame/04-NumberBaseball/index.js
+++ _study/zerocho/js-es2020-webgame/04-NumberBaseball/index.js
@@ -6,8 +6,8 @@ let numbers = [];
 for (let n = 1; n < 10; n++) {
   numbers.push(n); // 0부터 9까지의 정수를 배열에 담는다.
 }
-let guessNumbers = []; // 임의의 숫자를 담을 배열 선언
 
+let guessNumbers = []; // 임의의 숫자를 담을 배열 선언
 for (let n = 0; n < 4; n++) {
   const index = Math.floor(Math.random() * numbers.length) // 0 ~ 9 정수에서 임의의 숫자를 뽑는다.
   guessNumbers.push(numbers[index]); // 임의의 숫자를 배열에 담는다.
@@ -15,37 +15,51 @@ for (let n = 0; n < 4; n++) {
 }
 console.log(guessNumbers);
 
+msg.textContent = '숫자야구, 네자리 숫자를 입력해';
+
 let count = 1;
 check.addEventListener('click', (e) => {
   e.preventDefault();
   const value = input.value // 입력된 숫자 4개를 가져온다. (타입: 문자열)
   if (value && value.length === 4) {
     if (guessNumbers.join('') === value) { // 입력된 숫자(문자열)와 임의의 숫자를 비교 같다면
-      logs.textContent = 'HomeRun!!!'; // 홈런이다.
+      msg.textContent = 'HomeRun!!!'; // 홈런이다.
       input.disabled = true;
+      check.disabled = true;
     } else {
-      //console.log('다르다');
-      let strike = 0;
-      let ball = 0;
-      for (const [aIndex, aNumber] of guessNumbers.entries()) {
-        for (const [iIndex, iString] of input.value.split('').entries()) {
-          if (aNumber === Number(iString)) {
-            if (aIndex === iIndex)
-              strike++;
-            else
-              ball++;
+      const value_arr = value.split('');
+      const set = new Set(value_arr);
+      if ([...set].length < 4) { // 중복 숫자를 확인
+        console.log('중복된 숫자가 있군요');
+        logs.append(`${input.value} : 중복 없는 숫자를 입력해`, document.createElement('br'));
+      } else {
+        //console.log('다르다');
+        let strike = 0;
+        let ball = 0;
+        for (const [aIndex, aNumber] of guessNumbers.entries()) {
+          for (const [iIndex, iString] of input.value.split('').entries()) {
+            if (aNumber === Number(iString)) {
+              if (aIndex === iIndex)
+                strike++;
+              else
+                ball++;
+            }
           }
         }
-      }
-      logs.append(`${input.value}: ${strike} strike ${ball} ball`, document.createElement('br'));
-      if (count < 10) {
-        // 원래의 로직대로 되어있으면 12번의 기회가 주어진다. 10번의 기회만 갖도록 수정함
-        count++;
-      } else {
-        logs.appendChild(document.createTextNode(`Game Over: ${guessNumbers.join('')}`));
-        input.disabled = true;
+        logs.append(`${input.value} : ${strike} strike ${ball} ball`, document.createElement('br'));
+        if (count < 10) {
+          // 원래의 로직대로 되어있으면 12번의 기회가 주어진다. 10번의 기회만 갖도록 수정함
+          count++;
+        } else {
+          logs.appendChild(document.createTextNode(`Game Over : ${guessNumbers.join('')}`));
+          input.disabled = true;
+          check.disabled = true;
+        }
       }
     }
+  } else {
+    console.log('네자리 숫자가 아니군요');
+    logs.append(`${input.value} : 네자리 숫자를 입력해`, document.createElement('br'));
   }
   // 입력창을 초기화
   input.value = '';
---
+++ _study/zerocho/js-es2020-webgame/05-Lotto/index.js
@@ -0,0 +1,67 @@
+// 길이가 45인 배열을 생성하고 1부터 1씩 증가시킨 정수를 배열에 추가
+const candidate = Array(45).fill().map((v, i) => i + 1);
+
+const shuffle = [];
+while (candidate.length > 0) {
+  // 배열의 길이 중 하나의 인덱스를 랜덤하게 선택
+  const random = Math.floor(Math.random() * candidate.length);
+  // 해당 인덱스의 정수를 배열에서 제거하고 변수에 저장
+  const spliceArray = candidate.splice(random, 1);
+  // 랜덤하게 추출된 정수를 shuffle 배열에 저장
+  shuffle.push(spliceArray[0]);
+}
+//console.log(shuffle);
+
+const winBalls = shuffle.slice(0, 6); // 추출된 정수 배열에서 앞의 5개 정수를 저장 (배열)
+const bonus = shuffle[6]; // 6번째 숫자를 보너스로 저장
+console.log(winBalls);
+console.log(bonus);
+
+const resultTag = document.querySelector('#result');
+/*
+for (let i = 0; i < 6; i++) { // 클로저 문제는 let 나오면서 X
+  setTimeout(() => {
+    const ball = document.createElement('div');
+    ball.className = 'ball b1';
+    colorize(winBalls[i], ball);
+    ball.textContent = winBalls[i];
+    resultTag.appendChild(ball);
+  }, (i + 1) * 800);
+} */
+winBalls.forEach((number, index) => {
+  setTimeout(() => {
+    const ball = document.createElement('div');
+    ball.className = 'ball b1';
+    colorize(number, ball);
+    ball.textContent = number;
+    resultTag.appendChild(ball);
+  }, (index + 1) * 800);
+});
+
+const bonusTag = document.querySelector('#bonus');
+setTimeout(() => {
+  const bonusBall = document.createElement('div');
+  bonusBall.className = 'ball b1';
+  colorize(bonus, bonusBall);
+  bonusBall.textContent = bonus;
+  bonusTag.appendChild(bonusBall);
+}, 6000);
+
+function colorize(number, tag) {
+  if (number <= 10) {
+    tag.style.color = 'white';
+    tag.style.backgroundColor = 'Gold';
+  } else if (number <= 20) {
+    tag.style.color = 'white';
+    tag.style.backgroundColor = 'CornflowerBlue';
+  } else if (number <= 30) {
+    tag.style.color = 'white';
+    tag.style.backgroundColor = 'Crimson';
+  } else if (number <= 40) {
+    tag.style.color = 'white';
+    tag.style.backgroundColor = 'DarkGrey';
+  } else {
+    tag.style.color = 'white';
+    tag.style.backgroundColor = 'SeaGreen';
+  }
+}
