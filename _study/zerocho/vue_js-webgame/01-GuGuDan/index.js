f35596a398485fcc561a6a4c4f99481d5ac9a4fc
anifilm <anifilm02@gmail.com>
2021-04-14 15:42:47 +0900

    study vue.js 2021-04-14

---
+++ _study/zerocho/vue_js-webgame/01-GuGuDan/index.js
@@ -0,0 +1,25 @@
+const app = new Vue({
+  el: '#root',
+  data: {
+    first: Math.ceil(Math.random() * 9),
+    second: Math.ceil(Math.random() * 9),
+    value: '',
+    result: '',
+  },
+  methods: {
+    onSubmitForm(e) {
+      e.preventDefault();
+      if (this.first * this.second === parseInt(this.value)) {
+        this.result = '정답';
+        this.first = Math.ceil(Math.random() * 9);
+        this.second = Math.ceil(Math.random() * 9);
+        this.value = '';
+        this.$refs.answer.focus();
+      } else {
+        this.result = '땡';
+        this.value = '';
+        this.$refs.answer.focus();
+      }
+    },
+  },
+});
---
+++ _study/zerocho/vue_js-webgame/02-WordRelay/index.js
@@ -0,0 +1,21 @@
+const app = new Vue({
+  el: '#root',
+  data: {
+    word: '제로초',
+    result: '',
+    value: '',
+  },
+  methods: {
+    onSubmitForm(e) {
+      e.preventDefault();
+      if (this.word[this.word.length - 1] == this.value[0]) {
+        this.result = '딩동댕';
+        this.word = this.value;
+      } else {
+        this.result = '땡';
+      }
+      this.value = '';
+      this.$refs.answer.focus();
+    }
+  },
+});
