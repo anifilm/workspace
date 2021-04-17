cc1e47363fee65e8324d92787990a5babf649894
anifilm <anifilm02@gmail.com>
2021-04-17 11:12:14 +0900

    Update make_folder2.py

--- python/_etc/make_folder2.py
+++ python/_etc/make_folder2.py
@@ -1,8 +1,8 @@
 import os
 
 # 폴더 만들기
-for i in range(1, 9):
-    for j in range(1, 13):
+for i in range(1, 11):
+    for j in range(1, 16):
         if j < 10:
             os.mkdir(str(i) + "-0" + str(j))
         else:
