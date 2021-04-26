c1fcfdaeb61944c7af60b4ae24ba4597a81b18b6
anifilm <anifilm02@gmail.com>
2021-04-26 21:38:39 +0900

    study javascript 2021-04-26

---
+++ javascript/modern_js_Deep-Dive/chap11/11-04/index.js
@@ -0,0 +1,12 @@
+var str = 'string';
+
+// 문자열은 유사 배열이므로 배열과 유사하게 인덱스를 사용해 각 문자에 접근할 수 있다.
+console.log(str[0]); // "s"
+
+// 원시 값인 문자열이 객체처럼 동작한다.
+console.log(str.length); // 6
+console.log(str.toUpperCase()); // "STRING"
+
+/*
+원시 값을 객체처럼 사용하면 원시 값을 감싸는 래퍼 객체로 자동 변환된다.
+*/
---
+++ javascript/modern_js_Deep-Dive/chap11/11-06/index.js
@@ -0,0 +1,44 @@
+// 예제 11-06
+// 값에 의한 전달 (값 복사 전달)
+var score = 80;
+var copy = score;
+
+console.log(score); // 80
+console.log(copy);  // 80
+
+score = 100;
+
+console.log(score); // 100
+console.log(copy);  // 80
+
+
+// 예제 11-07
+var score = 80;
+
+// copy 변수에는 score 변수의 값 80이 복사되어 할단된다.
+var copy = score;
+
+console.log(score, copy); // 80 80
+console.log(score === copy); // true
+
+/*
+이때 score 변수와 copy 변수는 숫자 값 80을 갖는다는 점에서는 동일하다.
+하지만 score 변수와 copy 변수의 값 80은 다른 메모리 공간에 저장된 별개의 값이다.
+*/
+
+
+// 예제 11-08
+var score = 80;
+
+// copy 변수에는 score 변수의 값 80이 복사되어 할당된다.
+var copy = score;
+
+console.log(score, copy); // 80 80
+console.log(score === copy); // true
+
+// score 변수와 copy 변수의 값은 다른 메모리 공간에 저장된 별개의 값이다.
+// 따라서 score 변수의 값을 변경해도 copy 변수의 값에는 어떠한 영향도 주지 않는다.
+score = 100;
+
+console.log(score, copy); // 100 80
+console.log(score === copy); // false
