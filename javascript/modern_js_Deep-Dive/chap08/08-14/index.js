ae8c707356408095467d17dcb47901fdd228f13f
anifilm <anifilm02@gmail.com>
2021-04-26 13:59:48 +0900

    study javascript 2021-04-26

---
+++ javascript/modern_js_Deep-Dive/chap08/08-14/index.js
@@ -0,0 +1,23 @@
+// 예제 08-14
+var count = 0;
+
+// count가 3보다 작을 때까지 코드 블록을 계속 반복 실행한다.
+while (count < 3) {
+  console.log(count); // 0 1 2
+  count++;
+}
+
+// 예제 08-15
+// 무한루프
+//while (true) {}
+
+// 예제 08-16
+var count = 0;
+
+// 무한루프
+while (true) {
+  console.log(count); // 0 1 2
+  count++;
+  // count가 3이면 코드 블록을 탈출한다.
+  if (count === 3) break;
+}
