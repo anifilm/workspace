7d445c7684831e0ff5f1924391c15e58983c1421
anifilm <anifilm02@gmail.com>
2021-04-28 13:57:37 +0900

    study javascript 2021-04-28

---
+++ javascript/modern_js_Deep-Dive/chap13/13-01/index.js
@@ -0,0 +1,9 @@
+function add(x, y) {
+  // 매개변수는 함수 몸체 내부에서만 참조할 수 있다.
+  // 즉, 매개변수의 스코프(유효번위)는 함수 몸체 내부다.
+  console.log(x, y); // 2 5
+  return x + y;
+}
+add(2, 5);
+// 매개변수는 함수 몸체 내부에서만 참조할 수 있다.
+//console.log(x, y); // ReferenceError: x is not defined
