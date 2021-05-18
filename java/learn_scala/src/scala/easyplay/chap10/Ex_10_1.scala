1f341e46beec51d961f7c97a396cb4aa80bea9af
anifilm <anifilm02@gmail.com>
2021-05-18 19:49:37 +0900

    study scala 2021-05-18

---
+++ java/learn_scala/src/scala/easyplay/chap10/Ex_10_1.scala
@@ -0,0 +1,16 @@
+package scala.easyplay.chap10
+
+// 코드 10-1 정규표현식과 패턴 매칭
+object Ex_10_1 {
+  def convertInputs(input: String): Option[Seq[Int]] = {
+    val REGEX = "([0-9])([0-9])([0-9])".r
+    input match {
+      case REGEX(n1, n2, n3) =>
+        if (n1 != n2 && n2 != n3 && n1 != n3)
+          Some(Seq(n1.toInt, n2.toInt, n3.toInt))
+        else
+          None
+      case _ => None
+    }
+  }
+}
