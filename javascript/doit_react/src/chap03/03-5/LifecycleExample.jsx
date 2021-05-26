17cb1982bf80ecbc46cccb1148023097c86f8159
anifilm <anifilm02@gmail.com>
2021-05-26 11:22:01 +0900

    study react 2021-05-26

---
+++ javascript/doit_react/src/chap03/03-5/LifecycleExample.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class LifecycleExample extends Component {
+  static getDerivedStateFromProps() {
+    console.log('getDerivedStateFromProps 호출');
+    return {};
+  }
+  constructor(props) {
+    super(props);
+    // getDerivedStateFromProps() 함수를 사용하므로
+    // 경고 메시지를 건너뛰기 위해 state 초기값을 설정합니다.
+    this.state = {};
+    console.log('constructor 호출');
+  }
+  componentDidMount() {
+    console.log('componentDidMount 호출');
+  }
+  componentDidUpdate() {
+    console.log('componentDidUpdate 호출');
+  }
+  componentWillUnmount() {
+    console.log('componentWillUnmount 호출');
+  }
+  getSnapshotBeforeUpdate() {
+    console.log('getSnapshotBeforeUpdate 호출');
+  }
+  shouldComponentUpdate() {
+    console.log('shouldComponentUpdate 호출');
+    return true;
+  }
+  render() {
+    console.log('render 호출');
+    return null;
+  }
+}
+
+export default LifecycleExample;
