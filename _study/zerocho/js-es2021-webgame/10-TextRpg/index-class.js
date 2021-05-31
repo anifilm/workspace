68c82d9ecdffdf28a1b578821b8b00668d548dac
anifilm <anifilm02@gmail.com>
2021-05-31 20:00:16 +0900

    study javascript 2021-05-31

--- _study/zerocho/js-es2021-webgame/10-TextRpg/index-class.js
+++ _study/zerocho/js-es2021-webgame/10-TextRpg/index-class.js
@@ -2,12 +2,10 @@ const $startScreen = document.querySelector('#start-screen');
 const $gameMenu = document.querySelector('#game-menu');
 const $battleMenu = document.querySelector('#battle-menu');
 const $heroName = document.querySelector('#hero-name');
-
 const $heroLevel = document.querySelector('#hero-level');
 const $heroHp = document.querySelector('#hero-hp');
 const $heroXp = document.querySelector('#hero-xp');
 const $heroAtt = document.querySelector('#hero-att');
-
 const $monsterName = document.querySelector('#monster-name');
 const $monsterHp = document.querySelector('#monster-hp');
 const $monsterAtt = document.querySelector('#monster-att');
