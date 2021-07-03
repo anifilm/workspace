fc9822441013e718b494e56f1357e8171ab80368
anifilm <anifilm02@gmail.com>
2021-07-03 14:56:01 +0900

    study vue.js 2021-07-03

---
+++ _study/memi-dev/vue-firebase-my-web-site/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  plugins: [
+    'vue' // 세미콜론 규칙 설정
+  ],
+  rules: {
+    'semi': ['error', 'always'], // 세미콜론 규칙 설정
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
