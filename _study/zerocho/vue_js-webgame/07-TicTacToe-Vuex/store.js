b3ee3d68dc2b4176da1642cd2bfb5912eb9cd51e
anifilm <anifilm02@gmail.com>
2021-07-15 22:01:18 +0900

    study vue.js 2021-07-15

---
+++ _study/zerocho/vue_js-webgame/07-TicTacToe-Vuex/store.js
@@ -0,0 +1,20 @@
+import Vuex from 'vuex';
+
+export default new Vuex.Store({
+  // data
+  state: {
+
+  },
+  // computed
+  mutations: {
+
+  },
+  // state를 수정할 때 사용 (동기적으로)
+  getters: {
+
+  },
+  // 비동기를 사용하거나 여러 뮤테이션을 연달아 실행할 때
+  actions: {
+
+  }
+});
