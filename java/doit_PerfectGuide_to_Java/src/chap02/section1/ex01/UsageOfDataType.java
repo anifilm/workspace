274cf481b42101137161703fd885c2711a2bf813
anifilm <anifilm02@gmail.com>
2021-09-02 19:36:55 +0900

    study java 2021-09-02

---
+++ java/doit_PerfectGuide_to_Java/src/chap02/section1/ex01/UsageOfDataType.java
@@ -0,0 +1,14 @@
+// 변수 선언과 값을 대입하는 2가지 방법
+package chap02.section1.ex01;
+
+public class UsageOfDataType {
+    public static void main(String[] args) {
+        // 변수 선언과 함께 값 대입
+        int a = 3;
+        // 변수 선언과 값 대입 분리
+        int b;
+        b = 4;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
---
+++ java/doit_PerfectGuide_to_Java/src/chap02/section2/ex03/RangeOFVariableUse.java
@@ -0,0 +1,16 @@
+// 변수의 생존 기간
+package chap02.section2.ex03;
+
+public class RangeOFVariableUse {
+    public static void main(String[] args) {
+        int value1 = 3; // 변수 value1 생성
+        {
+            int value2 = 5; // 변수 value2 생성
+            System.out.println(value1);
+            System.out.println(value2);
+        } // 변수 value2 소멸 시점
+
+        System.out.println(value1);
+     // System.out.println(value2); // 오류
+    } // 변수 value1 소멸 시점
+}
