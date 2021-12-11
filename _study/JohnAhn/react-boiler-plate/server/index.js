568af99bb7c6b00f2644ca3aa4f493a6c06cf056
anifilm <anifilm02@gmail.com>
2021-12-11 22:51:18 +0900

    study react 2021-12-11

--- _study/JohnAhn/react-boiler-plate/server/index.js
+++ _study/JohnAhn/react-boiler-plate/server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const app = express();
+
+const path = require('path');
+const cors = require('cors');
 
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-
-const { User } = require('./models/User');
 
 const config = require('./config/key');
 
@@ -13,6 +15,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
     //useCreateIndex: true,
+    //useFindAndModify: false,
   })
   .then(() => {
     console.log('MongoDB connecting Success!!');
@@ -21,8 +24,7 @@ mongoose
     console.log(err);
   });
 
-const app = express();
-const port = process.env.PORT || '3000';
+app.use(cors());
 
 // to not get any deprecation warning or error
 // support parsing of application/x-www-form-urlencoded post data
@@ -34,50 +36,25 @@ app.use(cookieParser());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.post('/register', (req, res) => {
-  // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
-  // 그것들을 데이터베이스에 넣어준다.
-  const user = new User(req.body);
+app.use('/api/users', require('./routes/users'));
 
-  user.save((err, userInfo) => {
-    if (err) return res.json({ success: false, err });
-    return res.status(200).json({
-      success: true,
-    });
+// use this to show the image you have in node js server to client (react js)
+// https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
+app.use('/uploads', express.static('uploads'));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  // All the javascript and css files will be read and served from this folder
+  app.use(express.static('client/build'));
+
+  // index.html for all page routes    html or routing and naviagtion
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
   });
-});
+}
 
-app.post('/login', (req, res) => {
-  // 요청된 이메일을 데이터베이스에 있는지 찾는다.
-  User.findOne({ email: req.body.email }, (err, user) => {
-    if (!user) {
-      return res.json({
-        loginSuccess: false,
-        message: '제공된 이메일에 해당하는 유저가 없습니다.',
-      });
-    }
-
-    // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
-    user.comparePassword(req.body.password, (err, isMatch) => {
-      if (!isMatch)
-        return res.json({
-          loginSuccess: false,
-          message: '비밀번호가 틀렸습니다.',
-        });
-
-      // 비밀번호까지 맞다면 토큰을 생성
-      user.generateToken((err, user) => {
-        if (err) return res.status(400).send(err);
-
-        // 토큰을 저장한다. (어디에? 쿠키 또는 세션, 로컬스토리지)
-        res.cookie('x_auth', user.token).status(200).json({
-          loginSuccess: true,
-          userId: user._id,
-        });
-      });
-    });
-  });
-});
+const port = process.env.PORT || '3000';
 
 app.listen(port, () =>
   console.log(
--- _study/JohnAhn/react-boiler-plate/server/models/User.js
+++ _study/JohnAhn/react-boiler-plate/server/models/User.js
@@ -83,8 +83,11 @@ userSchema.methods.generateToken = function (cb) {
 userSchema.statics.findByToken = function (token, cb) {
   const user = this;
 
+  // 토큰을 decode 한다.
   jwt.verify(token, process.env.JWT_SECRET, function (err, decode) {
-    user.findOne({ _id: decode, token: token }, function (err, user) {
+    // 유저 아이디를 이용해서 유저를 찾은 다음
+    // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+    user.findOne({ '_id': decode, 'token': token }, function (err, user) {
       if (err) return cb(err);
       cb(null, user);
     });
