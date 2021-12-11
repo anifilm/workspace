b0244771a0162762bd03ffb55fef4119390ebc4f
anifilm <anifilm02@gmail.com>
2021-12-11 22:23:56 +0900

    Update User.js

--- _study/JohnAhn/react-boiler-plate/server/models/User.js
+++ _study/JohnAhn/react-boiler-plate/server/models/User.js
@@ -80,6 +80,17 @@ userSchema.methods.generateToken = function (cb) {
   });
 };
 
+userSchema.statics.findByToken = function (token, cb) {
+  const user = this;
+
+  jwt.verify(token, process.env.JWT_SECRET, function (err, decode) {
+    user.findOne({ _id: decode, token: token }, function (err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
+  });
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = { User };
