ec9075c6f5296354000e5f86b9de3d2644f75326
anifilm <anifilm02@gmail.com>
2021-12-11 12:22:26 +0900

    study react 2021-12-11

--- _study/JohnAhn/react-boiler-plate/server/index.js
+++ _study/JohnAhn/react-boiler-plate/server/index.js
@@ -1,8 +1,34 @@
 const express = require('express');
 
+require('dotenv').config();
+
+const userId = process.env.USER_ID;
+const userPw = process.env.USER_PW;
+
+const mongoose = require('mongoose');
+mongoose
+  .connect(
+    `mongodb+srv://${userId}:${userPw}@cluster0.hpa4f.mongodb.net/react-boiler-plate?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      //useCreateIndex: true,
+    },
+  )
+  .then(() => {
+    console.log('MongoDB connecting Success!!');
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
 const app = express();
 const port = process.env.PORT || '3000';
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`The server is running, please open your browser at http://localhost:${port}\n`));
+app.listen(port, () =>
+  console.log(
+    `The server is running, please open your browser at http://localhost:${port}\n`,
+  ),
+);
---
+++ _study/JohnAhn/react-boiler-plate/server/models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const moment = require('moment');
+
+require('dotenv').config();
+const jwt = require(process.env.JWT_SECRET);
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = { User };
