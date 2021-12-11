fd86a71745c8d9ab3cbaf6ca83c09745d7b83c0e
anifilm <anifilm02@gmail.com>
2021-12-11 22:22:03 +0900

    study react 2021-12-11

--- _study/JohnAhn/react-boiler-plate/server/index.js
+++ _study/JohnAhn/react-boiler-plate/server/index.js
@@ -47,6 +47,38 @@ app.post('/register', (req, res) => {
   });
 });
 
+app.post('/login', (req, res) => {
+  // 요청된 이메일을 데이터베이스에 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: '제공된 이메일에 해당하는 유저가 없습니다.',
+      });
+    }
+
+    // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: '비밀번호가 틀렸습니다.',
+        });
+
+      // 비밀번호까지 맞다면 토큰을 생성
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // 토큰을 저장한다. (어디에? 쿠키 또는 세션, 로컬스토리지)
+        res.cookie('x_auth', user.token).status(200).json({
+          loginSuccess: true,
+          userId: user._id,
+        });
+      });
+    });
+  });
+});
+
 app.listen(port, () =>
   console.log(
     `The server is running, please open your browser at http://localhost:${port}\n`,
--- _study/JohnAhn/react-boiler-plate/server/models/User.js
+++ _study/JohnAhn/react-boiler-plate/server/models/User.js
@@ -4,6 +4,8 @@ const saltRounds = 10;
 const jwt = require('jsonwebtoken');
 const moment = require('moment');
 
+require('dotenv').config();
+
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -34,6 +36,49 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+userSchema.pre('save', function (next) {
+  const user = this;
+  // 비밀번호가 수정될 때만 실행
+  if (user.isModified('password')) {
+    //console.log('password changed!');
+    // 비밀번호를 암호화 시킨다.
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  const user = this;
+  console.log('user: ', user);
+  console.log('userSchema: ', userSchema);
+
+  const token = jwt.sign(user._id.toHexString(), process.env.JWT_SECRET);
+  const oneHour = moment().add(1, 'hour').valueOf();
+
+  user.tokenExp = oneHour;
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
 
 const User = mongoose.model('User', userSchema);
 
