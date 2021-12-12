203c11f29d2025fd037f331c6efd7e7ddf33d7ff
anifilm <anifilm02@gmail.com>
2021-12-12 21:25:12 +0900

    study react 2021-12-12

--- _study/JohnAhn/react-boiler-plate/server/index.js
+++ _study/JohnAhn/react-boiler-plate/server/index.js
@@ -54,7 +54,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '5000';
 
 app.listen(port, () =>
   console.log(
