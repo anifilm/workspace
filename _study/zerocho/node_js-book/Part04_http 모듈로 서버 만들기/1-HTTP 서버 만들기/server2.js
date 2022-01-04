0774ee7f15c503ddfcd70cfbae0e4e13e78d9e49
anifilm <anifilm02@gmail.com>
2022-01-04 20:55:40 +0900

    study node.js 2022-01-04

---
+++ _study/zerocho/node_js-book/Part04_http 모듈로 서버 만들기/1-HTTP 서버 만들기/server2.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  res.write('<h1>Hello Node!</h1>');
+  res.end('<p>Hello Server!</p>');
+});
+
+server.listen(8080);
+
+server.on('listening', () => {
+  console.log('8080포트에서 서버 대기중...');
+});
+
+server.on('error', (err) => {
+  console.error(err);
+});
