60e53ca688166a1a06a016a856b1e20c5b9f867f
anifilm <anifilm02@gmail.com>
2022-01-08 13:24:52 +0900

    Update pages.js

--- _study/zerocho/node_js-book/Part09_익스프레스로 SNS 서비스 만들기/1-노드버드 프로젝트 구조 갖추기/routes/pages.js
+++ _study/zerocho/node_js-book/Part09_익스프레스로 SNS 서비스 만들기/1-노드버드 프로젝트 구조 갖추기/routes/pages.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.use((req, res, next) => {
   res.locals.user = null;
   res.locals.followerCount = 0;
-  res.locals.followerCount = 0;
+  res.locals.followingCount = 0;
   res.locals.followerIdList = [];
   next();
 });
