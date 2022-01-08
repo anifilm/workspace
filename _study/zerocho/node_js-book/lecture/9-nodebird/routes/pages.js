1a840daeaa37c6db0494148c8f6375b95546760a
anifilm <anifilm02@gmail.com>
2022-01-08 13:24:49 +0900

    Update pages.js

--- _study/zerocho/node_js-book/lecture/9-nodebird/routes/pages.js
+++ _study/zerocho/node_js-book/lecture/9-nodebird/routes/pages.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.use((req, res, next) => {
   res.locals.user = req.user;
   res.locals.followerCount = 0;
-  res.locals.followerCount = 0;
+  res.locals.followingCount = 0;
   res.locals.followerIdList = [];
   next();
 });
