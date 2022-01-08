d83861525ced178d216206093af17c829d4f2d89
anifilm <anifilm02@gmail.com>
2022-01-08 10:49:02 +0900

    study node.js 2022-01-08

---
+++ _study/zerocho/node_js-book/lecture/9-nodebird/app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const pagesRouter = require('./routes/pages');
+
+const app = express();
+
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+// 템플릿 엔진
+app.set('view engine', 'html');
+nunjucks.configure(path.join(__dirname, 'views'), {
+  express: app,
+  watch: true,
+});
+
+// 미들웨어
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+}));
+
+// 라우터
+app.use('/', pagesRouter);
+
+// 404 Not Found
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(port, () => {
+  console.log(`The server is running, please open your browser at http://localhost:${port}\n`);
+});
--- _study/zerocho/node_js-book/lecture/9-nodebird/models/index.js
+++ _study/zerocho/node_js-book/lecture/9-nodebird/models/index.js
@@ -1,29 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+
 const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
+const db = {};
 
 const dotenv = require('dotenv');
 dotenv.config();
 
-const User = require('./user');
-const Comment = require('./comment');
-
-const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config')[env];
 config.password = process.env.MYSQL_USER_PW;
 
-const db = {};
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+fs.readFileSync(__dirname).filter((file) => {
+  return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+}).forEach((file) => {
+  var model = sequelize['import'](path.join(__dirname, file));
+  db[model.name] = model;
+})
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
-db.User = User;
-db.Comment = Comment;
-
-User.init(sequelize);
-Comment.init(sequelize);
-
-User.associate(db);
-Comment.associate(db);
 
 module.exports = db;
