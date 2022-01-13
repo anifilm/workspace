5d03dc8c870f46fa5891e976b940127b5f39b639
anifilm <anifilm02@gmail.com>
2022-01-13 16:19:14 +0900

    study react 2022-01-13

--- _study/zerocho/react-nodebird/lecture/front/components/AppLayout.js
+++ _study/zerocho/react-nodebird/lecture/front/components/AppLayout.js
@@ -14,7 +14,7 @@ const SearchInput = styled(Input.Search)`
 `;
 
 const AppLayout = ({ children }) => {
-  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
+  const { isLoggedIn } = useSelector((state) => state.user);
 
   return (
     <div>
