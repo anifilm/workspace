d8989c3da46739ce3b370fd5d970ceae8030baca
anifilm <anifilm02@gmail.com>
2022-01-14 13:05:16 +0900

    study react 2022-01-14

---
+++ _study/zerocho/react-nodebird/Part02_Redux 연동하기/10-이미지 캐루셀 구현하기(react-slick)/components/PostImages.js
@@ -0,0 +1,87 @@
+import React, { useCallback, useState } from 'react';
+import PropTypes from 'prop-types';
+
+import { PlusOutlined } from '@ant-design/icons';
+
+import ImagesZoom from './ImagesZoom';
+
+const PostImages = ({ images }) => {
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
+
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true);
+  }, []);
+  const onClose = useCallback(() => {
+    setShowImagesZoom(false);
+  }, []);
+
+  if (images.length === 1) {
+    return (
+      <>
+        <img
+          role="presentation"
+          src={`${images[0].src}`}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+      </>
+    );
+  }
+  if (images.length === 2) {
+    return (
+      <>
+        <img
+          role="presentation"
+          src={`${images[0].src}`}
+          alt={images[0].src}
+          onClick={onZoom}
+          style={{ width: '50%', display: 'inline-block' }}
+        />
+        <img
+          role="presentation"
+          src={`${images[1].src}`}
+          alt={images[1].src}
+          onClick={onZoom}
+          style={{ width: '50%', display: 'inline-block' }}
+        />
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+      </>
+    );
+  }
+  return (
+    <>
+      <div>
+        <img
+          role="presentation"
+          src={`${images[0].src}`}
+          alt={images[0].src}
+          onClick={onZoom}
+          style={{ width: '50%' }}
+        />
+        <div
+          role="presentation"
+          onClick={onZoom}
+          style={{
+            display: 'inline-block',
+            width: '50%',
+            textAlign: 'center',
+            verticalAlign: 'middle',
+          }}
+        >
+          <PlusOutlined />
+          <br />
+          {images.length - 1}
+          개의 사진 더보기
+        </div>
+      </div>
+      {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+    </>
+  );
+};
+
+PostImages.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.object),
+};
+
+export default PostImages;
