d80d78cb4e50a7aaedc1240f329f8a4f6821f60f
anifilm <anifilm02@gmail.com>
2022-01-18 13:09:59 +0900

    Update Movie.js

--- _study/nomadcoders/react_MovieApp/react-movie-app/src/components/Movie.js
+++ _study/nomadcoders/react_MovieApp/react-movie-app/src/components/Movie.js
@@ -10,13 +10,7 @@ function Movie({ id, year, title, summary, poster, genres }) {
       <Link
         to={{
           pathname: `/movie/${id}`,
-          state: {
-            year,
-            title,
-            summary,
-            poster,
-            genres
-          }
+          state: { year, title, summary, poster, genres },
         }}
       >
         <img src={poster} alt={title} title={title} />
@@ -25,9 +19,7 @@ function Movie({ id, year, title, summary, poster, genres }) {
           <h5 className="movie__year">{year}</h5>
           <ul className="movie__genres">
             {genres.map((genre, index) => (
-              <li key={index} className="genres__genre">
-                {genre}
-              </li>
+              <li key={index} className="genres__genre">{genre}</li>
             ))}
           </ul>
           <p className="movie__summary">{summary.slice(0, 180)}...</p>
