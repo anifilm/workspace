af91f2c90236d85c95f3551cff190c89df10df24
anifilm <anifilm02@gmail.com>
2022-01-18 19:24:45 +0900

    Update server.js

--- _study/nomadcoders/vanilla_js_ZoomClone/src/server.js
+++ _study/nomadcoders/vanilla_js_ZoomClone/src/server.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => res.render('home'));
-app.get('*', (req, res) => res.redirect('/'));
+app.get('/*', (req, res) => res.redirect('/'));
 
 const handleListen = () => {
   console.log(`Listening on http://localhost:3000`);
