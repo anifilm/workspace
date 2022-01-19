2670988312b2e654d079d10e3aa455abd82dc069
anifilm <anifilm02@gmail.com>
2022-01-19 11:26:04 +0900

    자료 삭제

--- _study/nomadcoders/vanilla_js_ChromeApp/2_3_modify_DOM/index.js
+++
@@ -1,5 +0,0 @@
-// const title = document.getElementById("title");
-const title = document.querySelector("#title");
-title.innerHTML = "Hello, From JS!";
-title.style.color = "red";
-document.title = "Modifying the DOM with JS";
