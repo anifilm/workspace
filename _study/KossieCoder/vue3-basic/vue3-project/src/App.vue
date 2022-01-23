b99ce6c031f60b5bc0370285f33d36b0cc42c191
anifilm <anifilm02@gmail.com>
2022-01-23 13:30:49 +0900

    Update App.vue

--- _study/KossieCoder/vue3-basic/vue3-project/src/App.vue
+++ _study/KossieCoder/vue3-basic/vue3-project/src/App.vue
@@ -1,3 +1,20 @@
 <template>
-  <div>Kossie coder</div>
+  <div class="name">{{ name }}</div>
 </template>
+
+<script>
+export default {
+  setup() {
+    const name = 'Kossie Coder1';
+    return {
+      name
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .name {
+    color: red;
+  }
+</style>
