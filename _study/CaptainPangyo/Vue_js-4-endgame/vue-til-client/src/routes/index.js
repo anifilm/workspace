8b53f558c26ea7d9b061323b9bcd6286955ec556
anifilm <anifilm02@gmail.com>
2022-01-25 20:42:50 +0900

    study vue.js 2022-01-25
    Vue.js 끝장내기 학습

---
+++ _study/CaptainPangyo/Vue_js-4-endgame/vue-til-client/src/routes/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import LoginPage from '@/views/LoginPage.vue';
+import SignupPage from '@/views/SignupPage.vue';
+//import NotFoundPage from '@/views/NotFoundPage.vue';
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      component: LoginPage
+    },
+    {
+      path: '/signup',
+      component: SignupPage
+    },
+    {
+      path: '*',
+      component: () => import('@/views/NotFoundPage.vue')
+    }
+  ]
+});
