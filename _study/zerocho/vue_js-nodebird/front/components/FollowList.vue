8c53d1e1790c0a5046275c87907e07dacaf27c3a
anifilm <anifilm02@gmail.com>
2022-01-30 07:39:57 +0900

    Update FollowList.vue

--- _study/zerocho/vue_js-nodebird/front/components/FollowList.vue
+++ _study/zerocho/vue_js-nodebird/front/components/FollowList.vue
@@ -1,23 +1,40 @@
 <template>
   <v-list>
-    <v-list-item>
-      <span>제로초</span>
-      <v-icon>mdi-minus-circle-outline</v-icon>
-    </v-list-item>
-    <v-list-item>
-      <span>네로</span>
-      <v-icon>mdi-minus-circle-outline</v-icon>
-    </v-list-item>
-    <v-list-item>
-      <span>히로</span>
-      <v-icon>mdi-minus-circle-outline</v-icon>
-    </v-list-item>
+    <v-col
+      v-for="user in users"
+      v-bind:key="user.id"
+      cols="12"
+      md="4"
+      style="display: inline-block"
+    >
+      <v-list-item>
+        <v-list-item-avatar color="indigo">
+          <span class="white--text headling">{{ user.nickname[0] }}</span>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title>{{ user.nickname }}</v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-icon v-on:click="remove(user.id)">mid-minus-circle-outline</v-icon>
+        </v-list-item-action>
+      </v-list-item>
+    </v-col>
   </v-list>
 </template>
 
 <script>
 export default {
-  //
+  props: {
+    abc: Array,
+    users: {
+      type: Array,
+      required: true,
+    },
+    remove: {
+      type: Function,
+      required: true,
+    },
+  },
 };
 </script>
 
