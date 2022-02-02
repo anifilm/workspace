b311e382d8bd809ab567bc8fc56d50f79e6bf6e0
anifilm <anifilm02@gmail.com>
2022-02-02 21:00:10 +0900

    Update user.js

--- _study/zerocho/redux-vs-mobx/lecture/10-redux-saga/front/sagas/user.js
+++ _study/zerocho/redux-vs-mobx/lecture/10-redux-saga/front/sagas/user.js
@@ -1,5 +1,9 @@
 import { all, fork, call, put, takeLatest } from 'redux-saga/effects';
-import { LOG_IN, LOG_IN_FAILURE, LOG_IN_SUCCESS } from '../reducers/user';
+import {
+  LOG_IN_REQUEST,
+  LOG_IN_SUCCESS,
+  LOG_IN_FAILURE,
+} from '../reducers/user';
 
 function loginAPI() {
   // 서버에 요청을 보내는 부분
@@ -8,10 +12,12 @@ function loginAPI() {
 function* login() {
   try {
     yield call(loginAPI);
-    yield put({ // put은 dispatch와 동일
+    yield put({
+      // put은 dispatch와 동일
       type: LOG_IN_SUCCESS,
     });
-  } catch (err) { // loginAPI 실패
+  } catch (err) {
+    // loginAPI 실패
     console.error(err);
     yield put({
       type: LOG_IN_FAILURE,
@@ -20,11 +26,9 @@ function* login() {
 }
 
 function* watchLogin() {
-  yield takeLatest(LOG_IN, login);
+  yield takeLatest(LOG_IN_REQUEST, login);
 }
 
 export default function* userSaga() {
-  yield all([
-    fork(watchLogin),
-  ]);
+  yield all([fork(watchLogin)]);
 }
