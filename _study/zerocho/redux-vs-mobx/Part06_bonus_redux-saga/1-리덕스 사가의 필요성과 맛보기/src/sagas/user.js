109e69a8070be59f08f155dce211dcd60147c97c
anifilm <anifilm02@gmail.com>
2022-02-02 20:00:04 +0900

    study redux-saga 2022-02-02

---
+++ _study/zerocho/redux-vs-mobx/Part06_bonus_redux-saga/1-리덕스 사가의 필요성과 맛보기/src/sagas/user.js
@@ -0,0 +1,28 @@
+import { all, fork, call, put, takeLatest } from 'redux-saga/effects';
+import { LOG_IN, LOG_IN_FAILURE, LOG_IN_SUCCESS } from '../reducers/user';
+
+function loginAPI() {
+  // 서버에 요청을 보내는 부분
+}
+
+function* login() {
+  try {
+    yield call(loginAPI);
+    yield put({ // put은 dispatch와 동일
+      type: LOG_IN_SUCCESS,
+    });
+  } catch (err) { // loginAPI 실패
+    console.error(err);
+    yield put({
+      type: LOG_IN_FAILURE,
+    });
+  }
+}
+
+function* watchLogin() {
+  yield takeLatest(LOG_IN, login);
+}
+
+export default function* userSaga() {
+  yield all([fork(watchLogin)]);
+}
