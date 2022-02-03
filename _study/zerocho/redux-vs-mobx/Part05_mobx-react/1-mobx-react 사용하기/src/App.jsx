204be6637001d42f886d38c766ce53d46c1149e1
anifilm <anifilm02@gmail.com>
2022-02-03 19:48:29 +0900

    study mobx 2022-02-03

---
+++ _study/zerocho/redux-vs-mobx/Part05_mobx-react/1-mobx-react 사용하기/src/App.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { observer } from 'mobx-react';
+import { observable } from 'mobx';
+
+import { userStore, postStore } from './store';
+
+class App extends Component {
+  state = observable({
+    name: '',
+    password: '',
+  });
+
+  onLogin = () => {
+    userStore.logIn({
+      nickname: 'zerocho',
+      password: 'password',
+    });
+  };
+
+  onLogout = () => {
+    userStore.logOut();
+  };
+
+  onChangeName = (e) => {
+    this.state.name = e.target.value;
+  };
+
+  onChangePassword = (e) => {
+    this.state.password = e.target.value;
+  };
+
+  render() {
+    return (
+      <div>
+        {userStore.isLoggingIn ? (
+          <div>로그인 중...</div>
+        ) : userStore.data ? (
+          <div>{userStore.data.nickname}님 안녕하세요.</div>
+        ) : (
+          <div>로그인 해주세요.</div>
+        )}
+        {!userStore.data ? (
+          <button onClick={this.onLogin}>로그인</button>
+        ) : (
+          <button onClick={this.onLogout}>로그아웃</button>
+        )}
+        <div>{postStore.data.length}</div>
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={this.onChangeName}
+        />
+        <input
+          type="password"
+          value={this.state.password}
+          onChange={this.onChangePassword}
+        />
+      </div>
+    );
+  }
+}
+
+export default observer(App);
---
+++ _study/zerocho/redux-vs-mobx/Part05_mobx-react/1-mobx-react 사용하기/src/store.js
@@ -0,0 +1,24 @@
+import { observable } from 'mobx';
+
+export const userStore = observable({
+  isLoggingIn: false,
+  data: null,
+  logIn(data) {
+    this.isLoggingIn = true;
+    setTimeout(() => {
+      this.data = data;
+      this.isLoggingIn = false;
+      postStore.data.push(1);
+    }, 2000);
+  },
+  logOut() {
+    this.data = null;
+  },
+});
+
+export const postStore = observable({
+  data: [],
+  addPost(data) {
+    this.data.push(data);
+  },
+});
