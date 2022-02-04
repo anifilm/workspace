8220b62d44367541c38eb0b074a44b1a42d5a88f
anifilm <anifilm02@gmail.com>
2022-02-04 15:01:20 +0900

    study react 2022-02-04

--- _study/zerocho/react-nodebird/Part03_Redux-saga 연동하기/13-팔로우, 언팔로우 구현하기/components/PostCard.js
+++ _study/zerocho/react-nodebird/Part03_Redux-saga 연동하기/13-팔로우, 언팔로우 구현하기/components/PostCard.js
@@ -97,11 +97,7 @@ const PostCard = ({ post }) => {
               <li>
                 <Comment
                   author={item.User.nickname}
-                  avatar={(
-                    <Link href={{ pathname: '/user', query: { id: item.User.id } }} as={`/user/${item.User.id}`}>
-                      <a><Avatar>{item.User.nickname[0]}</Avatar></a>
-                    </Link>
-                  )}
+                  avatar={<Avatar>{item.User.nickname[0]}</Avatar>}
                   content={item.content}
                 />
               </li>
