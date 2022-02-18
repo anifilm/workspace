ae48c1947d3960fb36fb9a04499151fa83d1e622
anifilm <anifilm02@gmail.com>
2022-02-18 12:24:11 +0900

    study vue.js 3 2022-02-18

---
+++ _study/onnote/vue_js_3/vue3-tutorial/3-vue_js_3/src/components/ItemMain.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <li v-for="item in items" v-bind:key="item.name">
+      {{ item.name }}의 가격:
+      <input type="text" v-on:input="item.price = $event.target.value" v-bind:value="item.price" />
+    </li>
+  </ul>
+  <hr />
+  <item-list v-bind:items="items"></item-list>
+</template>
+
+<script>
+import ItemList from './ItemList.vue';
+
+export default {
+  name: 'ItemMain',
+  components: {
+    ItemList,
+  },
+  // props 속성 정의
+  props: {
+    items: {
+      type: Object,
+      default: () => {
+        return [];
+      },
+    },
+  },
+};
+</script>
--- _study/onnote/vue_js_3/vue3-tutorial/3-vue_js_3/src/main.js
+++ _study/onnote/vue_js_3/vue3-tutorial/3-vue_js_3/src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue';
-import ItemMain from './ItemMain.vue'; // ItemMain 컴포넌트 임포트
-// 애플리케이션 인스턴스 생성
-createApp(ItemMain).mount('#app');
+import App from './App.vue';
+
+createApp(App).mount('#app');
