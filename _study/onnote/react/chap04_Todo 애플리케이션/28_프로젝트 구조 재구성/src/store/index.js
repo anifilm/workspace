b2d2f35c26c201d6a2baa0950b7662581c6cb7f8
anifilm <anifilm02@gmail.com>
2022-02-21 11:39:16 +0900

    study react 2022-02-21

--- _study/onnote/react/chap04_Todo 애플리케이션/28_프로젝트 구조 재구성/src/store/index.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-export default function configureStore(reducer) {
-  return createStore(reducer, composeWithDevTools());
-}
