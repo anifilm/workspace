41e3d67e0e6a6e0bb13a10c5dfca7196013849e6
anifilm <anifilm02@gmail.com>
2022-02-21 13:03:22 +0900

    study react 2022-02-21
    Chap04 Todo 애플리케이션
    24. 선택자 함수

---
+++ _study/onnote/react/chap04_Todo 애플리케이션/24_선택자 함수/src/modules/todos.js
@@ -0,0 +1,98 @@
+import { createAction, handleActions } from 'redux-actions';
+
+// 액션 타입
+const ADD_TODO = 'ADD_TODO';
+const TOGGLE_TODOS_STATUS = 'TOGGLE_TODO_STATUS';
+const CHANGE_TODO_INPUT = 'CHANGE_TODO_INPUT';
+const REMOVE_TODO = 'REMOVE_TODO';
+const CLEAR_ALL_TODOS = 'CLEAR_ALL_TODOS';
+const RESTORE = 'RESTORE';
+const CHANGE_FILTER = 'CHANGE_FILTER';
+
+// createAction 함수를 활용하여 액션 생성 함수 작성
+export const addTodo = createAction(ADD_TODO, (input) => ({
+  text: input,
+  done: false,
+}));
+export const toggleTodoStatus = createAction(TOGGLE_TODOS_STATUS, (id) => id);
+export const changeTodoInput = createAction(CHANGE_TODO_INPUT, (input) => input);
+export const removeTodo = createAction(REMOVE_TODO, (id) => id);
+export const clearAllTodos = createAction(CLEAR_ALL_TODOS);
+export const restore = createAction(RESTORE, (data) => data);
+export const changeFilter = createAction(CHANGE_FILTER, (filter) => filter);
+
+// 초기 상태
+const initialState = {
+  input: '',
+  todos: [
+    {
+      id: 1,
+      text: 'todoItem1',
+      done: true,
+    },
+    {
+      id: 2,
+      text: 'todoItem2',
+      done: false,
+    },
+    {
+      id: 3,
+      text: 'todoItem3',
+      done: false,
+    },
+  ],
+  nextTodoId: 4,
+  filter: 'ALL',
+};
+
+// handleActions 함수를 활용하여 리듀서 작성
+const todos = handleActions(
+  {
+    [ADD_TODO]: (state, { payload: todo }) => {
+      const newTodo = { ...todo, id: state.nextTodoId };
+      const nextTodoId = state.nextTodoId + 1;
+      return {
+        ...state,
+        todos: state.todos.concat(newTodo),
+        nextTodoId,
+      };
+    },
+    [TOGGLE_TODOS_STATUS]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo,
+      ),
+    }),
+    [CHANGE_TODO_INPUT]: (state, { payload: input }) => ({
+      ...state,
+      input,
+    }),
+    [REMOVE_TODO]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.filter((todo) => todo.id !== id),
+    }),
+    [CLEAR_ALL_TODOS]: (state, action) => ({
+      ...state,
+      todos: state.todos.filter((todo) => !todo.done),
+    }),
+    // 상태 복원 액션 처리
+    [RESTORE]: (state, action) => {
+      //console.log(action);
+      //console.log(action.payload.todos);
+      //console.log(action.payload.nextTodoId);
+      return ({
+        ...state,
+        todos: action.payload.todos,
+        nextTodoId: action.payload.nextTodoId,
+      });
+    },
+    // 필터링 유형 변경 액션 처리
+    [CHANGE_FILTER]: (state, { payload: filter }) => ({
+      ...state,
+      filter,
+    }),
+  },
+  initialState,
+);
+
+export default todos;
