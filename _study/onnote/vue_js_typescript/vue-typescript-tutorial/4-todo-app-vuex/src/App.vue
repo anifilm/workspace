6ea6801881d0e89a0ecfd10c5f184ca224d50e02
anifilm <anifilm02@gmail.com>
2022-02-21 19:20:38 +0900

    study vue.js 2022-02-21

---
+++ _study/onnote/vue_js_typescript/vue-typescript-tutorial/4-todo-app-vuex/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <TodoHeader />
+    <TodoInput v-on:addTodo="addTodo" />
+    <TodoList
+      v-bind:todos="todos"
+      v-on:checkTodo="checkTodo"
+      v-on:removeTodo="removeTodo"
+    />
+    <TodoFooter v-on:clearAll="clearAll" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import store from './store';
+
+import TodoHeader from './components/TodoHeader.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoList from './components/TodoList.vue';
+import TodoFooter from './components/TodoFooter.vue';
+
+@Component({
+  components: {
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter,
+  },
+})
+export default class App extends Vue {
+  // 스토어 상태 접근
+  get todoItems() {
+    return store.state.todos;
+  }
+
+  addTodo(todo: string) {
+    store.dispatch('addTodo', todo);
+  }
+  checkTodo(id: number) {
+    store.dispatch('checkTodo', id);
+  }
+  removeTodo(index: number) {
+    store.dispatch('removeTodo', index);
+  }
+  clearAll() {
+    store.dispatch('clearAll');
+  }
+}
+</script>
---
+++ _study/onnote/vue_js_typescript/vue-typescript-tutorial/4-todo-app-vuex/src/components/TodoInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <input type="text" v-model="newTodo" v-on:keypress.enter="addTodo" />
+    <button v-on:click="addTodo">추가</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class TodoInput extends Vue {
+  newTodo: string = '';
+
+  addTodo() {
+    if (this.newTodo !== '') {
+      const value = this.newTodo && this.newTodo.trim();
+      console.log('addTodo', value);
+      this.$emit('addTodo', value);
+      this.clearInput();
+    }
+  }
+  clearInput() {
+    this.newTodo = '';
+  }
+}
+</script>
+
+<style scoped>
+div {
+  background-color: Pink;
+}
+button {
+  margin: 5px;
+}
+</style>
