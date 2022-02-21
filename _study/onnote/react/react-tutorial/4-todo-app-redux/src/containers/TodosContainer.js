df2bbcac6570d99cc7b3a1300883811dd4759d8d
anifilm <anifilm02@gmail.com>
2022-02-21 11:36:12 +0900

    Update TodosContainer.js

--- _study/onnote/react/react-tutorial/4-todo-app-redux/src/containers/TodosContainer.js
+++ _study/onnote/react/react-tutorial/4-todo-app-redux/src/containers/TodosContainer.js
@@ -32,36 +32,7 @@ const TodosContainer = ({
   );
 };
 
-/* 스토어 상태를 props로 맵핑
-const mapStateToProps = (state) => ({
-  input: state.input,
-  todos: state.todos,
-});
-
-// 스토어 상태 변경 함수를 props로 맵핑
-const mapDispatchToProps = (dispatch) => ({
-  addTodo: (input) => {
-    dispatch(addTodo(input));
-  },
-  toggleTodoStatus: (id) => {
-    dispatch(toggleTodoStatus(id));
-  },
-  changeTodoInput: (input) => {
-    dispatch(changeTodoInput(input));
-  },
-  removeTodo: (id) => {
-    dispatch(removeTodo(id));
-  },
-  clearAllTodos: () => {
-    dispatch(clearAllTodos());
-  },
-});
-
-// 리덕스와 연동된 컴포넌트 반환
-export default connect(mapStateToProps, mapDispatchToProps)(TodosContainer);
-*/
-
-/* 익명 함수 처리
+// 익명 함수 처리
 export default connect(
   (state) => ({
     input: state.input,
@@ -74,40 +45,4 @@ export default connect(
     removeTodo: (id) => dispatch(removeTodo(id)),
     clearAllTodos: () => dispatch(clearAllTodos()),
   }),
-)(TodosContainer);
-*/
-
-/* bindActionsCreator 유틸 함수 사용
-export default connect(
-  (state) => ({
-    input: state.input,
-    todos: state.todos,
-  }),
-  (dispatch) =>
-    bindActionCreators(
-      {
-        addTodo,
-        toggleTodoStatus,
-        changeTodoInput,
-        removeTodo,
-        clearAllTodos,
-      },
-      dispatch,
-    ),
-)(TodosContainer);
-*/
-
-// 객체 형태 처리
-export default connect(
-  (state) => ({
-    input: state.input,
-    todos: state.todos,
-  }),
-  {
-    addTodo,
-    toggleTodoStatus,
-    changeTodoInput,
-    removeTodo,
-    clearAllTodos,
-  },
 )(TodosContainer);
