bd4c0abfbb6649849d48b872474a6557e9a53448
anifilm <anifilm02@gmail.com>
2022-02-22 13:48:23 +0900

    study vue.js 2022-02-22

---
+++ _study/onnote/vue_js/chap04_Todo 애플리케이션/15_Todo 항목 컴포넌트 정의/src/components/TodoItem.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <li>
+      <input
+        type="checkbox"
+        v-bind:checked="todo.done"
+        v-on:change="checkTodo"
+      />
+      <span v-if="!isEditing" v-on:dblclick="handleDblClick">{{
+        todo.text
+      }}</span>
+      <input
+        v-else
+        type="text"
+        ref="content"
+        v-bind:value="todo.text"
+        v-on:blur="handleBlur"
+        v-on:keydown.enter="updateTodo"
+      />
+      <button v-on:click="removeTodo">삭제</button>
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: {
+    todo: {
+      type: Object,
+    },
+    editingId: {
+      type: Number,
+    },
+  },
+  computed: {
+    isEditing() {
+      return this.todo.id === this.editingId;
+    },
+  },
+  methods: {
+    checkTodo() {
+      const id = this.todo.id;
+      console.log('checkTodo', id);
+      this.$emit('checkTodo', id);
+    },
+    updateTodo(e) {
+      const id = this.todo.id
+      console.log('update Todo', id);
+      const content = e.target.value.trim();
+      if (content.length <= 0) return false;
+      this.$emit('updateTodo', id, content);
+      this.$refs.content.blur();
+    },
+    removeTodo() {
+      const id = this.todo.id
+      console.log('removeTodo', id);
+      this.$emit('removeTodo', id);
+    },
+    handleDblClick() {
+      const id = this.todo.id
+      this.$emit('setEditingId', id);
+      this.$nextTick(() => {
+        this.$refs.content.focus();
+      });
+    },
+    handleBlur() {
+      this.$emit('resetEditingId');
+    },
+  },
+};
+</script>
+
+<style scoped>
+span, input {
+  margin: 10px;
+}
+li {
+  background-color: LightYellow;
+  margin: 10px 0;
+}
+</style>
