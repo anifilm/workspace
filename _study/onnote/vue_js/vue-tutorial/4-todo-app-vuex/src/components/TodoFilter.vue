5ef47c12d7ef126d7052b4d3e38c3f31c667353e
anifilm <anifilm02@gmail.com>
2022-02-22 17:52:38 +0900

    study vue.js 2022-02-22

---
+++ _study/onnote/vue_js/vue-tutorial/4-todo-app-vuex/src/components/TodoFilter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input
+      type="radio"
+      v-bind:checked="filter === 'ALL'"
+      v-on:change="SET_FILTER('ALL')"
+    />전체
+    <input
+      type="radio"
+      v-bind:checked="filter === 'NOTDONE'"
+      v-on:change="SET_FILTER('NOTDONE')"
+    />미완료
+    <input
+      type="radio"
+      v-bind:checked="filter === 'DONE'"
+      v-on:change="SET_FILTER('DONE')"
+    />완료
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex';
+import { SET_FILTER } from '../store/mutation-types';
+
+export default {
+  computed: {
+    ...mapState(['filter']),
+  },
+  methods: {
+    ...mapMutations([SET_FILTER])
+  }
+};
+</script>
