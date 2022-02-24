726b5ff34c5d3fbdd477bded4df36e5f108109ee
anifilm <anifilm02@gmail.com>
2022-02-24 14:08:02 +0900

    study react 2022-02-24

--- _study/onnote/react_typescript/chap07_리덕스 적용/6_리덕스 모듈/src/modules/board.ts
+++ _study/onnote/react_typescript/chap07_리덕스 적용/6_리덕스 모듈/src/modules/board.ts
@@ -1,5 +1,30 @@
 import { createReducer } from 'typesafe-actions';
+import { createAction } from 'redux-actions';
 import { Board } from '../App';
+
+// 액션 타입
+const FETCH = 'board/FETCH';
+const FETCH_SUCCESS = 'board/FETCH_SUCCESS';
+const FETCH_FAILURE = 'board/FETCH_FAILURE';
+
+const FETCH_LIST = 'board/FETCH_LIST';
+const FETCH_LIST_SUCCESS = 'board/FETCH_LIST_SUCCESS';
+const FETCH_LIST_FAILURE = 'board/FETCH_LIST_FAILURE';
+
+const CHANGE_TITLE = 'board/CHANGE_TITLE';
+const CHANGE_CONTENT = 'board/CHANGE_CONTENT';
+
+// 액션 생성 함수
+export const fetchStart = createAction(FETCH);
+export const fetchSuccess = createAction(FETCH_SUCCESS, (data: string) => data);
+export const fetchFailure = createAction(FETCH_FAILURE, (err: any) => err);
+
+export const fetchListStart = createAction(FETCH_LIST);
+export const fetchListSuccess = createAction(FETCH_LIST_SUCCESS, (data: string) => data);
+export const fetchListFailure = createAction(FETCH_LIST_FAILURE, (err: any) => err);
+
+export const changeTitle = createAction(CHANGE_TITLE, (title: string) => title);
+export const changeContent = createAction(CHANGE_CONTENT, (content: string) => content);
 
 // 상태 인터페이스 정의
 export interface BoardState {
@@ -32,7 +57,67 @@ const initialState: BoardState = {
 // 리듀서 함수 정의
 const board = createReducer(
   initialState,
-  {},
+  {
+    [FETCH]: (state) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        FETCH: true,
+      },
+    }),
+    [FETCH_SUCCESS]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        FETCH: false,
+      },
+      board: action.payload,
+    }),
+    [FETCH_FAILURE]: (state) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        FETCH: false,
+      },
+    }),
+    [FETCH_LIST]: (state) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        FETCH_LIST: true,
+      },
+    }),
+    [FETCH_LIST_SUCCESS]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        FETCH_LIST: false,
+      },
+      boards: action.payload,
+    }),
+    [FETCH_LIST_FAILURE]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        FETCH_LIST: false,
+      },
+      error: action.payload,
+    }),
+    [CHANGE_TITLE]: (state, { payload: title }) => ({
+      ...state,
+      board: {
+        ...state.board,
+        title,
+      },
+    }),
+    [CHANGE_CONTENT]: (state, { payload: content }) => ({
+      ...state,
+      board: {
+        ...state.board,
+        content,
+      },
+    }),
+  },
 );
 
 export default board;
