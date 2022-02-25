4d4ac2e38abae088ac02291e7bb39de19d4949ab
anifilm <anifilm02@gmail.com>
2022-02-25 21:05:31 +0900

    Update BoardReadPage.vue

--- _study/onnote/vue_js/chap05_CRUD 게시판/4_라우터 정의/src/pages/BoardReadPage.vue
+++ _study/onnote/vue_js/chap05_CRUD 게시판/4_라우터 정의/src/pages/BoardReadPage.vue
@@ -3,7 +3,7 @@
     <h3>게시글 상세보기</h3>
     <board-read />
     <router-link
-      :to="{ name: 'BoardModifyPage', params: { BoardNo: 100 } }"
+      :to="{ name: 'BoardModifyPage', params: { boardNo: 100 } }"
     >수정</router-link>
     <button>삭제</button>
     <router-link :to="{ name: 'BoardListPage' }">목록</router-link>
