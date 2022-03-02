3dd73dca01a1e815fa9ea2bf4d0a66d102a9f1cc
anifilm <anifilm02@gmail.com>
2022-03-02 10:27:13 +0900

    study vue.js 2022-03-02

---
+++ _study/onnote/vue_js/chap07_실전 프로젝트/7_최초 관리자 생성/src/components/member/AdminSetupForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <table>
+      <tr>
+        <td>관리자 아이디</td>
+        <td><input type="text" v-model="userId" /></td>
+      </tr>
+      <tr>
+        <td>관리자명</td>
+        <td><input type="text" v-model="userName" /></td>
+      </tr>
+      <tr>
+        <td>비밀번호</td>
+        <td><input type="password" v-model="userPw" /></td>
+      </tr>
+      <tr>
+        <td colspan="2" align="center">
+          <button type="submit">등록</button>
+        </td>
+      </tr>
+    </table>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'AdminSetupForm',
+  data() {
+    return {
+      userId: '',
+      userName: '',
+      userPw: '',
+    };
+  },
+  methods: {
+    onSubmit() {
+      const { userId, userName, userPw } = this;
+      this.$emit('submit', { userId, userName, userPw });
+    },
+  },
+};
+</script>
---
+++ _study/onnote/vue_js/chap07_실전 프로젝트/7_최초 관리자 생성/src/pages/member/AdminSetupPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div align="center">
+    <h2>최초 관리자 등록</h2>
+    <admin-setup-form @submit="onSubmit" />
+  </div>
+</template>
+
+<script>
+import api from '@/api';
+import AdminSetupForm from '@/components/member/AdminSetupForm.vue';
+
+export default {
+  name: 'AdminSetupPage',
+  components: {
+    AdminSetupForm,
+  },
+  methods: {
+    onSubmit(payload) {
+      const { userId, userName, userPw } = payload;
+      api
+        .post('/users/setup', { userId, userName, userPw })
+        .then((res) => {
+          alert('등록이 완료되었습니다.');
+          this.$router.push({
+            name: 'Home',
+          });
+        })
+        .catch((err) => {
+          alert(err.response.data);
+        });
+    },
+  },
+};
+</script>
