c4feb6b9c1e2dcdd8ff82037d1370a948faa2786
anifilm <anifilm02@gmail.com>
2022-03-04 10:22:55 +0900

    study react 2022-03-04

--- _study/onnote/react/chap09_실전 프로젝트/7_최초 관리자 생성/src/App.js
+++ _study/onnote/react/chap09_실전 프로젝트/7_최초 관리자 생성/src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import HomePage from './pages/HomePage';
-import SignInPage from './pages/auth/SignInPage';
-import AdminSetupPage from './pages/member/AdminSetupPage';
+import HomePage from 'pages/HomePage';
+import SignInPage from 'pages/auth/SignInPage';
+import AdminSetupPage from 'pages/member/AdminSetupPage';
 
-const App = () => {
+function App() {
   return (
-    <div>
+    <>
       <Route path="/" component={HomePage} exact />
       <Route path="/signin" component={SignInPage} exact />
       <Route path="/member/setup" component={AdminSetupPage} />
-    </div>
+    </>
   );
 }
 
--- _study/onnote/react/react-tutorial/9-image-shop/src/App.js
+++ _study/onnote/react/react-tutorial/9-image-shop/src/App.js
@@ -3,12 +3,14 @@ import { Route } from 'react-router-dom';
 
 import HomePage from 'pages/HomePage';
 import SignInPage from 'pages/auth/SignInPage';
+import AdminSetupPage from 'pages/member/AdminSetupPage';
 
 function App() {
   return (
     <>
       <Route path="/" component={HomePage} exact />
       <Route path="/signin" component={SignInPage} exact />
+      <Route path="/member/setup" component={AdminSetupPage} />
     </>
   );
 }
