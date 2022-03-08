f0785e64ba4115d25b839f69f3e81a473f74b156
anifilm <anifilm02@gmail.com>
2022-03-08 13:23:50 +0900

    study javascript 2022-03-08

---
+++ _study/nomadcoders/vanilla_js_ChromeApp/Part04_Login/app.js
@@ -0,0 +1,15 @@
+const loginInput = document.querySelector('#login-form input');
+const loginButton = document.querySelector('#login-form button');
+
+function onLoginBtnClick() {
+  const username = loginInput.value;
+  if (username === '') {
+    alert('Please write your name.');
+  } else if (username.length > 15) {
+    alert('Your name is too long.');
+  } else {
+    console.log(username);
+  }
+}
+
+loginButton.addEventListener('click', onLoginBtnClick);
