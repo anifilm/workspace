ff0794dc51e61aff3da9db75ae0cfe1fca30bb02
anifilm <anifilm02@gmail.com>
2022-03-08 14:59:09 +0900

    study javascript 2022-03-08

--- _study/nomadcoders/vanilla_js_ChromeApp/Part04_Login/app.js
+++ _study/nomadcoders/vanilla_js_ChromeApp/Part04_Login/app.js
@@ -1,18 +1,26 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
 
 const link = document.querySelector('a');
+
+const HIDDEN_CLASSNAME = 'hidden';
 
 function onLoginSubmit(e) {
   e.preventDefault();
   const username = loginInput.value;
   if (username === '') {
     alert('Please write your name.');
+    return;
   } else if (username.length > 15) {
     alert('Your name is too long.');
+    return;
   } else {
-    console.log(username);
+    console.log(`Hello ${username}`);
   }
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function handleLinkClick() {
