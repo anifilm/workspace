bd54de6b5321fbec921a556c3c0bed9af709edba
anifilm <anifilm02@gmail.com>
2022-03-10 21:53:27 +0900

    study javascript 2022-03-10

---
+++ _study/nomadcoders/vanilla_js_ChromeApp/Part08_Weather/js/weather.js
@@ -0,0 +1,22 @@
+const weather = document.querySelector('#weather span:first-child');
+const city = document.querySelector('#weather span:last-child');
+const API_KEY = '54c406d2d0cbdf21665daf77415ee218';
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  console.log(lat, lon);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
