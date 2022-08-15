2c873481727607db8f361a39981ce4d99e507b15
anifilm <anifilm02@gmail.com>
2022-08-15 12:23:45 +0900

    study java 2022-08-15

---
+++ java/java_basic/chap_pre/Exam10.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Exam10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("숫자를 입력하고 엔터를 치세요: ");
+        int num = sc.nextInt();
+        System.out.println("당신이 입력한 숫자는: " + num);
+        sc.close();
+    }
+}
