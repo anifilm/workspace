030ffcd122d949ac7deb0396f55e829c70298957
anifilm <anifilm02@gmail.com>
2022-08-16 20:42:20 +0900

    study c# 2022-08-16

---
+++ csharp/csharp_book/chap34/34-12_ExpressionBodiedConstructor.cs
@@ -0,0 +1,21 @@
+using System;
+
+// 식 본문 생성자(Expression Bodied Constructor)
+class Pet {
+    private string _name;
+
+    // Expression Bodied Constructor
+    public Pet(string name) =>
+        _name = name;
+
+    public override string ToString() {
+        return _name;
+    }
+}
+
+class ExpressionBodiedConstructor {
+    static void Main() {
+        var pet = new Pet("야옹이");
+        Console.WriteLine(pet.ToString());
+    }
+}
