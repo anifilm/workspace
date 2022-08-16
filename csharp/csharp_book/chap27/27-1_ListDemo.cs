756ca3666207b68a45a9b643f5938d8ccf8d682a
anifilm <anifilm02@gmail.com>
2022-08-16 18:48:21 +0900

    study c# 2022-08-16

---
+++ csharp/csharp_book/chap27/27-1_ListDemo.cs
@@ -0,0 +1,17 @@
+using System;
+
+// 리스트(배열, 컬렉션)
+class ListDemo {
+    static void Main() {
+        string[] colors = { "red", "green", "blue" };
+        Console.WriteLine(colors[0]);  // red
+        Console.WriteLine(colors[1]);  // green
+        Console.WriteLine(colors[2]);  // blue
+
+        Console.WriteLine("요소 수: {0}", colors.Length);  // 3
+
+        for (int i = 0; i < colors.Length; i++) {
+            Console.WriteLine(colors[i]);
+        }
+    }
+}
---
+++ csharp/csharp_book/chap27/27-5_StackClass.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+
+class StackClass {
+    static void Main() {
+        Stack stack = new Stack();
+        stack.Push("First");
+        stack.Push("Second");
+
+        Console.WriteLine(stack.Pop());
+        Console.WriteLine(stack.Pop());
+        //Console.WriteLine(stack.Pop());
+    }
+}
