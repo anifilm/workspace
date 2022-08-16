04926209f5b5c29fb2014f9f30bdd22c1ddccf51
anifilm <anifilm02@gmail.com>
2022-08-16 19:45:45 +0900

    study c# 2022-08-16

---
+++ csharp/csharp_book/chap56/56-5_AsyncAwaitDescription.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class AsyncAwaitDescription {
+    static void Main() {
+        Task.Run(() => DoPrint());  // 1, 2, 3, ...
+        Console.WriteLine("async await 사용 예제");
+        Thread.Sleep(1);  // 1 밀리초 대기
+        //Console.ReadLine();
+    }  // end Main
+
+    static async void DoPrint() {
+        await PrintNumberAsync();
+    }
+
+    static async Task PrintNumberAsync() {
+        await Task.Run(() => {
+            // 오래 걸리는 작업 들어오는 곳
+            for (int i = 0; i < 300; i++) {
+                Console.WriteLine(i + 1);
+            }
+        });
+    }
+}
