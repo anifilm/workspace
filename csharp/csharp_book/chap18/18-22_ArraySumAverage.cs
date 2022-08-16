a7ccca97449e6f503e6664a7cfaae15c2d2950bb
anifilm <anifilm02@gmail.com>
2022-08-16 12:22:04 +0900

    Create 18-22_ArraySumAverage.cs

---
+++ csharp/csharp_book/chap18/18-22_ArraySumAverage.cs
@@ -0,0 +1,21 @@
+using System;
+
+int[,] scores = {
+    { 90, 100, 0, 0 },
+    { 80, 90, 0, 0 },
+    { 100, 80, 0, 0 }
+};
+
+for (int i = 0; i < 3; i++) {
+    scores[i, 2] = scores[i, 0] + scores[i, 1];  // SUM(합계)
+    scores[i, 3] = scores[i, 2] / 2;  // AVERAGE(평균)
+}
+
+Console.WriteLine(" 국어 영어 합계 평균");
+for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 4; j++) {
+        // 각 점수를 5칸 잡고 오른쪽 정렬해서 출력
+        Console.Write($"{scores[i, j],5}");
+    }
+    Console.WriteLine();
+}
