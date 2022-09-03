86c77610790a25b0118bd3872a9ade61a6c5c117
anifilm <anifilm02@gmail.com>
2022-09-03 23:10:14 +0900

    study java 2022-09-03

---
+++ java/java_basic/chap10/Ex03_MemoryInHeap2.java
@@ -0,0 +1,17 @@
+public class Ex03_MemoryInHeap2 {
+    public static void main(String[] args) {
+        Book book1 = new Book();  // [1] 객체 생성
+        book1.num = 10;
+
+        Book book2 = book1;  // [2] 객체에 객체 대입
+
+        System.out.println("book1.num : " + book1.num);
+        System.out.println("book2.num : " + book2.num);
+        System.out.println("--------------------");
+
+        book2.num = 20;
+
+        System.out.println("book1.num : " + book1.num);
+        System.out.println("book2.num : " + book2.num);
+    }
+}
