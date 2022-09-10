3c4b1b7b79e13ea1defd0068ff1d4ebeac32b7bc
anifilm <anifilm02@gmail.com>
2022-09-10 10:29:45 +0900

    study c# 2022-09-10

---
+++ csharp/csharp_book/chap30/30-31_FromWhereSelect.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+
+// LINQ(Language Integrated Query)
+class FromWhereSelect {
+    static void Main() {
+        int[] arr = { 1, 2, 3, 4, 5 };
+
+        var evenNumbers =
+            from num in arr
+            where num % 2 == 0
+            select num;
+
+        foreach (var number in evenNumbers) {
+            Console.WriteLine($"짝수: {number}");
+        }
+    }
+}
