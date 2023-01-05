f9e2dd4dc6e4747b2e4a0dd9c8d410bfd88b9514
임채영 <anifilm@katkit.co.kr>
2023-01-05 16:59:01 +0900

    study python 23-01-05

--- python/_etc/_test2.py
+++ python/_etc/_test2.py
@@ -15,10 +15,20 @@ for i in items:
     stock = i.select_one("p > span")
     if title is None or price is None:
         continue
-    print(title.get("title"))
-    print(str(price.contents[0]).strip().rjust(60))
-    if stock.get_text() != "통상 가격:":
-        print("\n" + str(stock.get_text()).rjust(55))
+
+    title_str = str(title.get("title"))
+    price_str = str(price.contents[0]).strip()[0:-2]
+    stock_str = str(stock.get_text())
+    print(title_str)
+    print(price_str.rjust(60))
+
+    if stock_str.find("통상") == -1:
+        print("\n" + stock_str.rjust(56))
     else:
         print()
+
+    # 해당 상품 구매가능한 경우 카카오 메시지 전송
+    if title_str.find("안드") != -1 and stock_str.find("품절") == -1:
+        print("구매가능!")
+
     print("-" * 60)
