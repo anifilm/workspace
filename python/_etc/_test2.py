a582d6c09ccb797f80b6ceaab82201b0b0055cca
임채영 <anifilm@katkit.co.kr>
2023-01-05 16:36:18 +0900

    study python 2023-01-05

--- python/_etc/_test2.py
+++ python/_etc/_test2.py
@@ -6,14 +6,19 @@ res = requests.get(url)
 res.raise_for_status()
 
 soup = BeautifulSoup(res.text, "lxml")
-item_list = soup.find('ul', attrs={"class": "products-grid"})
+item_list = soup.find('div', class_="category-products")
 items = item_list.find_all('li')
 
 for i in items:
-    title = i.find("h2")
-    if title is None:
+    title = i.find("a")
+    price = i.find("span", class_="price")
+    stock = i.select_one("p > span")
+    if title is None or price is None:
         continue
-    price = i.find("span", attrs={"class": "price"})
-    stock = i.find("p", attrs={"class": "out-of-stock"})
-    print(title, price, stock)
-    print("-----------------------------------------------------------")
+    print(title.get("title"))
+    print(str(price.contents[0]).strip().rjust(60))
+    if stock.get_text() != "통상 가격:":
+        print("\n" + str(stock.get_text()).rjust(55))
+    else:
+        print()
+    print("-" * 60)
