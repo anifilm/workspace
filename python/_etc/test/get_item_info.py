52613c3960cac727d70de3f38a78bcca3f4692e9
임채영 <anifilm@katkit.co.kr>
2023-01-11 10:37:34 +0900

    study python 2023-01-11

--- python/_etc/test/get_item_info.py
+++ python/_etc/test/get_item_info.py
@@ -18,22 +18,35 @@ def get_info(url):
 
         title_str = str(title.get("title"))
         price_str = str(price.contents[0]).strip()[0:-2]
-        print(title_str)
-        print(price_str.rjust(60))
 
         if stock is not None:
             stock_str = str(stock.get_text())
             if stock_str.find("통상") == -1:
-                print("\n" + stock_str.rjust(56))
+                # print("\n" + stock_str.rjust(56)) # 재고품절 출력
+                pass
             else:
-                print("\n")
-            # 해당 상품 구매가능한 경우 카카오 메시지 전송
-            if title_str.find("안드") != -1 and stock_str.find("품절") == -1:
-                print("구매가능!")
+                # 특정 상품 표시안함 (이노치노하하)
+                if title_str.find("이노치노하하") != -1:
+                    continue
+                # 품절이 아닌 경우 금액 출력
+                print(title_str)
+                print(price_str.rjust(60))
+                print("-" * 60)
+                # 해당 상품 구매가능한 경우 카카오 메시지 전송 (1)
+                if title_str.find("안드") != -1:
+                    print("구매가능!")
         else:
-            print("\n")
+            # 특정 상품 표시안함 (이노치노하하)
+            if title_str.find("이노치노하하") != -1:
+                continue
+            # 품절이 아닌 경우 금액 출력
+            print(title_str)
+            print(price_str.rjust(60))
+            print("-" * 60)
+            # 해당 상품 구매가능한 경우 카카오 메시지 전송 (2)
+            if title_str.find("안드") != -1:
+                print("구매가능!")
 
-        print("-" * 60)
 
 
 url1 = "https://bombyxdrug-xsrvjp.ssl-xserver.jp/bd/index.php/tedf.html?p=1"
