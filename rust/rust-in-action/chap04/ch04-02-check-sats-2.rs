6ba43aae98c1d78ff4d1bcb4ed4af7d00c5908dc
임채영 <anifilm@katkit.co.kr>
2023-01-19 16:39:34 +0900

    study rust 2023-01-19

---
+++ rust/rust-in-action/chap04/ch04-02-check-sats-2.rs
@@ -0,0 +1,32 @@
+#![allow(unused_variables)]
+
+#[derive(Debug)]
+struct CubeSat {
+    id: u64,
+}
+
+#[derive(Debug)]
+enum StatusMessage {
+    Ok,
+}
+
+fn check_status(sat_id: CubeSat) -> StatusMessage {
+    StatusMessage::Ok
+}
+
+fn main() {
+    let sat_a = CubeSat { id: 0 };
+    let sat_b = CubeSat { id: 1 };
+    let sat_c = CubeSat { id: 2 };
+
+    let a_status = check_status(sat_a); // sat_a 변수(구조체 자료형)의 소유권이 함수의 인자로 이동됨
+    let b_status = check_status(sat_b);
+    let c_status = check_status(sat_c);
+    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
+
+    // '대기 중' ...
+    let a_status = check_status(sat_a);
+    let b_status = check_status(sat_b);
+    let c_status = check_status(sat_c);
+    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
+}
