5071322028ce2a299ffe1b19da235dfc9c9798fe
anifilm <anifilm02@gmail.com>
2023-02-05 14:45:20 +0900

    자료 삭제

--- python/python_oop_practice/p_oop_2/chap05/cooperation/class_module/__init__.py
+++
@@ -1,4 +0,0 @@
-from .student import Student
-from .bus import Bus
-from .subway import Subway
-from .taxi import Taxi
--- python/python_oop_practice/p_oop_2/chap07/inheritance/vip_customer.py
+++
@@ -1,9 +0,0 @@
-from customer import Customer
-
-
-class VIPCustomer(Customer):
-    def __init__(self):
-        self.customer_grade = "VIP"
-        self.bonus_ratio = 0.05
-        self.sales_ratio = 0.1
-        self.agentID = 0
