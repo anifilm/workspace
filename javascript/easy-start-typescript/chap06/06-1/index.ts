8ee3752706a76c62fcdb9ccb12d66685488e4460
anifilm <anifilm02@gmail.com>
2023-07-14 20:43:39 +0900

    study typescript 2023-07-14

---
+++ javascript/easy-start-typescript/chap06/06-1/index.ts
@@ -0,0 +1,6 @@
+function logText(text: string | number) {
+  console.log(text);
+}
+
+logText('hi'); // hi
+logText(100); // 100
---
+++ javascript/easy-start-typescript/chap06/06-2/index.ts
@@ -0,0 +1,6 @@
+function logText(text: any) {
+  console.log(text.toString());
+}
+
+logText('hi'); // hi
+logText(100); // 100
