1dffedc73e604bb749f260aaf7d5ff33fec2eba7
anifilm <anifilm02@gmail.com>
2023-08-01 00:35:12 +0900

    remove files

--- csharp/charp_best_know-how/volume_2/chap01/01-1/Program.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-class Program {
-    static void Main(string[] args) {
-        int a;
-        a = 1 + 2;
-        Console.WriteLine(a);
-    }
-}
--- csharp/charp_best_know-how/volume_2/chap01/01-1/Program2.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-class Program {
-    static void Main(string[] args) {
-        int excellentChange = 1;
-        excellentChange += 2;
-        Console.WriteLine(excellentChange);
-    }
-}
