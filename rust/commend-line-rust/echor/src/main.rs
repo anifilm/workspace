97493b4f3026c0dbc973e92252cea414006f032d
anifilm <anifilm02@gmail.com>
2024-09-07 19:32:45 +0900

    study rust 2024-09-07

--- rust/commend-line-rust/echor/src/main.rs
+++ rust/commend-line-rust/echor/src/main.rs
@@ -1,3 +1,32 @@
+use clap::{Arg, ArgAction, Command};
+
 fn main() {
-    println!("{:?}", std::env::args());
+    let matches = Command::new("echor")
+        .version("0.1.0")
+        .author("Ken Youens-Clark <kyclark@gmail.com")
+        .about("Rust version of `echo`")
+        .arg(
+            Arg::new("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .num_args(1..),
+        )
+        .arg(
+            Arg::new("omit_newline")
+                .short('n')
+                .action(ArgAction::SetTrue)
+                .help("Do not print newline"),
+        )
+        .get_matches();
+
+    let text: Vec<String> = matches.get_many("text").unwrap().cloned().collect();
+    let omit_newline = matches.get_flag("omit_newline");
+
+    let mut ending = "\n";
+    if omit_newline {
+        ending = "";
+    }
+
+    print!("{}{ending}", text.join(" "));
 }
